ec17734a883545202f62a88dd39f93b257994371
George Amuzu <gnamuzu@gmail.com>
2024-03-10 16:21:28 +0000

    added TODO list

--- my_store/lib/main.dart
+++ my_store/lib/main.dart
@@ -3,5 +3,11 @@ import 'package:my_store/app.dart';
 import 'package:my_store/utils/theme/theme.dart';
 
 void main() {
+  // TODO: Add Widgets Binding
+  // TODO: Init Local Storage
+  // TODO: Await Native Splash
+  // TODO: Initialize Firebase
+  // TODO: Initialize Authentication
+
   runApp(const App());
 }
