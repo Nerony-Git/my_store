e91fc508e8642129a34df4ba98696c26f7fff235
George Amuzu <gnamuzu@gmail.com>
2024-03-10 23:32:09 +0000

    assigning names to images

---
+++ my_store/lib/utils/constants/images.dart
@@ -0,0 +1,13 @@
+class MyImages {
+  // -- App Logos
+  static const String darkAppSplashLogo = "assets/logos/splash-logo-black.png";
+  static const String lightAppSplashLogo = "assets/logos/splash-logo-white.png";
+
+  // -- OnBoarding Images
+  static const String onBoardingImage1 =
+      "assets/images/onboarding/sammy-line-searching.gif";
+  static const String onBoardingImage2 =
+      "assets/images/onboarding/sammy-line-shopping.gif";
+  static const String onBoardingImage3 =
+      "assets/images/onboarding/sammy-line-delivery.gif";
+}
