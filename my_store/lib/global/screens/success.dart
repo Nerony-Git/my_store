d8d5f34e96b351fefb336cebdc213c65ccfc551d
George Amuzu <gnamuzu@gmail.com>
2024-03-11 21:13:01 +0000

    modified screen to make it global

---
+++ my_store/lib/global/screens/success.dart
@@ -0,0 +1,67 @@
+import 'package:flutter/material.dart';
+import 'package:my_store/global/styles/padding_styles.dart';
+import 'package:my_store/utils/constants/sizes.dart';
+import 'package:my_store/utils/constants/texts.dart';
+import 'package:my_store/utils/helpers/helper_functions.dart';
+
+class SuccessScreen extends StatelessWidget {
+  const SuccessScreen(
+      {super.key,
+      required this.img,
+      required this.title,
+      required this.subTitle,
+      required this.onPressed});
+
+  final String img, title, subTitle;
+  final VoidCallback onPressed;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: PaddingStyle.paddingWithAppBarHeight * 2,
+          child: Column(
+            children: [
+              /// Image
+              Image(
+                image: AssetImage(img),
+                width: HelperFunctions.screenWidth() * 0.6,
+              ),
+              const SizedBox(
+                height: MySizes.spaceBtwSections,
+              ),
+
+              /// Title and subtitle
+              Text(
+                title,
+                style: Theme.of(context).textTheme.headlineMedium,
+                textAlign: TextAlign.center,
+              ),
+              const SizedBox(
+                height: MySizes.spaceBtwItems,
+              ),
+              Text(
+                subTitle,
+                style: Theme.of(context).textTheme.labelMedium,
+                textAlign: TextAlign.center,
+              ),
+              const SizedBox(
+                height: MySizes.spaceBtwSections,
+              ),
+
+              /// Buttons
+              SizedBox(
+                width: double.infinity,
+                child: ElevatedButton(
+                  onPressed: onPressed,
+                  child: const Text(MyText.tContinue),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
