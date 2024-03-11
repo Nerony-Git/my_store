c690308144ca685c43bfa0ce4d9864dcddc4f9ee
George Amuzu <gnamuzu@gmail.com>
2024-03-11 07:43:32 +0000

    assigned variables for social logos

--- my_store/lib/utils/constants/images.dart
+++ my_store/lib/utils/constants/images.dart
@@ -1,13 +1,17 @@
 class MyImages {
   // -- App Logos
-  static const String darkAppSplashLogo = "assets/logos/splash-logo-black.png";
-  static const String lightAppSplashLogo = "assets/logos/splash-logo-white.png";
+  static const String darkAppLogo = "assets/logos/splash-logo-black.png";
+  static const String lightAppLogo = "assets/logos/splash-logo-white.png";
 
   // -- OnBoarding Images
   static const String onBoardingImage1 =
-      "assets/images/onboarding/sammy-line-searching.gif";
+      "assets/img/onboarding/sammy-line-searching.gif";
   static const String onBoardingImage2 =
-      "assets/images/onboarding/sammy-line-shopping.gif";
+      "assets/img/onboarding/sammy-line-shopping.gif";
   static const String onBoardingImage3 =
-      "assets/images/onboarding/sammy-line-delivery.gif";
+      "assets/img/onboarding/sammy-line-delivery.gif";
+
+  // -- Social Logos
+  static const String google = "assets/icons/google.png";
+  static const String facebook = "assets/icons/facebook.png";
 }
