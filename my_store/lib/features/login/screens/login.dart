1bb969e731ba46e4632e94328d1e4f2ff15c5414
George Amuzu <gnamuzu@gmail.com>
2024-03-11 07:49:17 +0000

    added login screen

---
+++ my_store/lib/features/login/screens/login.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:my_store/features/login/styles/login_padding.dart';
+import 'package:my_store/features/login/widgets/login_footer.dart';
+import 'package:my_store/features/login/widgets/login_form.dart';
+import 'package:my_store/features/login/widgets/login_form_divider.dart';
+import 'package:my_store/features/login/widgets/login_header.dart';
+import 'package:my_store/utils/constants/sizes.dart';
+import 'package:my_store/utils/constants/texts.dart';
+
+class LoginScreen extends StatelessWidget {
+  const LoginScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: LoginPadding.paddingWithAppBarHeight,
+          child: Column(
+            children: [
+              /// Logo. Title and Sub Title
+              const LoginHeader(),
+
+              /// Form
+              const LoginForm(),
+
+              /// Divider
+              LoginFormDivider(
+                dividerText: MyText.orSignInWith.capitalize!,
+              ),
+              const SizedBox(
+                height: MySizes.spaceBtwSections,
+              ),
+
+              /// Footer
+              const LoginFooter(),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
