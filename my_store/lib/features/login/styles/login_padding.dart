e2dbc5ea528063a2de939fd35d08600fd2b505c2
George Amuzu <gnamuzu@gmail.com>
2024-03-11 07:48:53 +0000

    added login padding style

---
+++ my_store/lib/features/login/styles/login_padding.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:my_store/utils/constants/sizes.dart';
+
+class LoginPadding {
+  static const EdgeInsetsGeometry paddingWithAppBarHeight = EdgeInsets.only(
+    top: MySizes.appBarHeight,
+    left: MySizes.defaultSpace,
+    bottom: MySizes.defaultSpace,
+    right: MySizes.defaultSpace,
+  );
+}
