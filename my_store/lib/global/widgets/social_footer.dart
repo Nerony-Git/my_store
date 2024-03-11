c283e78bb32ba7ac38926121a6bdc48f214434e3
George Amuzu <gnamuzu@gmail.com>
2024-03-11 16:53:08 +0000

    made the social footer widget global

---
+++ my_store/lib/global/widgets/social_footer.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+import 'package:my_store/utils/constants/colors.dart';
+import 'package:my_store/utils/constants/images.dart';
+import 'package:my_store/utils/constants/sizes.dart';
+
+class SocialFooter extends StatelessWidget {
+  const SocialFooter({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Container(
+          decoration: BoxDecoration(
+            border: Border.all(color: MyColors.grey),
+            borderRadius: BorderRadius.circular(100),
+          ),
+          child: IconButton(
+            onPressed: () {},
+            icon: const Image(
+              width: MySizes.iconMd,
+              height: MySizes.iconMd,
+              image: AssetImage(MyImages.google),
+            ),
+          ),
+        ),
+        const SizedBox(
+          width: MySizes.spaceBtwItems,
+        ),
+        Container(
+          decoration: BoxDecoration(
+            border: Border.all(color: MyColors.grey),
+            borderRadius: BorderRadius.circular(100),
+          ),
+          child: IconButton(
+            onPressed: () {},
+            icon: const Image(
+              width: MySizes.iconMd,
+              height: MySizes.iconMd,
+              image: AssetImage(MyImages.facebook),
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
