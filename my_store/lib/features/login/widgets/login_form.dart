11b1255c6128e6df27b3a0101b217d227037fbfe
George Amuzu <gnamuzu@gmail.com>
2024-03-11 07:47:24 +0000

    added login form widget

---
+++ my_store/lib/features/login/widgets/login_form.dart
@@ -0,0 +1,92 @@
+import 'package:flutter/material.dart';
+import 'package:iconsax/iconsax.dart';
+import 'package:my_store/utils/constants/sizes.dart';
+import 'package:my_store/utils/constants/texts.dart';
+
+class LoginForm extends StatelessWidget {
+  const LoginForm({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Form(
+      child: Padding(
+        padding: const EdgeInsets.symmetric(vertical: MySizes.spaceBtwSections),
+        child: Column(
+          children: [
+            /// Email
+            TextFormField(
+              decoration: const InputDecoration(
+                prefixIcon: Icon(Iconsax.direct_right),
+                labelText: MyText.email,
+              ),
+            ),
+            const SizedBox(
+              height: MySizes.spaceBtwInputFields,
+            ),
+
+            /// Password
+            TextFormField(
+              decoration: const InputDecoration(
+                prefixIcon: Icon(Iconsax.password_check),
+                labelText: MyText.password,
+                suffixIcon: Icon(Iconsax.eye_slash),
+              ),
+            ),
+            const SizedBox(
+              height: MySizes.spaceBtwInputFields / 2,
+            ),
+
+            /// Remember Me and Forget Password
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                /// Remember me
+                Row(
+                  children: [
+                    Checkbox(
+                      value: true,
+                      onChanged: (value) {},
+                    ),
+                    const Text(MyText.rememberMe),
+                  ],
+                ),
+
+                /// Forget password
+                TextButton(
+                  onPressed: () {},
+                  child: const Text(MyText.forgetPassword),
+                ),
+              ],
+            ),
+            const SizedBox(
+              height: MySizes.spaceBtwSections,
+            ),
+
+            /// Sign in button
+            SizedBox(
+              width: double.infinity,
+              child: ElevatedButton(
+                onPressed: () {},
+                child: const Text(MyText.signIn),
+              ),
+            ),
+            const SizedBox(
+              height: MySizes.spaceBtwItems,
+            ),
+
+            /// Sign up button
+            SizedBox(
+              width: double.infinity,
+              child: ElevatedButton(
+                onPressed: () {},
+                child: const Text(MyText.createAccount),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
