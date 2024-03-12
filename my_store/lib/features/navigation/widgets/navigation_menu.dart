266d1f065023f3bed0b1f4461f96096bdc8b39af
George Amuzu <gnamuzu@gmail.com>
2024-03-12 06:30:57 +0000

    created Navigation Menu

---
+++ my_store/lib/features/navigation/widgets/navigation_menu.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:iconsax/iconsax.dart';
+import 'package:my_store/features/navigation/controller/navigation_controller.dart';
+import 'package:my_store/utils/constants/colors.dart';
+import 'package:my_store/utils/helpers/helper_functions.dart';
+
+class NavigationMenu extends StatelessWidget {
+  const NavigationMenu({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final controller = Get.put(NavigationController());
+    final dark = HelperFunctions.isDarkMode(context);
+
+    return Scaffold(
+      bottomNavigationBar: Obx(
+        () => NavigationBar(
+          height: 80,
+          elevation: 0,
+          selectedIndex: controller.selectedIndex.value,
+          onDestinationSelected: (index) =>
+              controller.selectedIndex.value = index,
+          backgroundColor: dark ? MyColors.black : Colors.white,
+          indicatorColor: dark
+              ? MyColors.white.withOpacity(0.1)
+              : MyColors.black.withOpacity(0.1),
+          destinations: const [
+            NavigationDestination(icon: Icon(Iconsax.home), label: 'Home'),
+            NavigationDestination(icon: Icon(Iconsax.shop), label: 'Store'),
+            NavigationDestination(icon: Icon(Iconsax.heart), label: 'WishList'),
+            NavigationDestination(icon: Icon(Iconsax.user), label: 'Profile'),
+          ],
+        ),
+      ),
+      body: Obx(() => controller.screens[controller.selectedIndex.value]),
+    );
+  }
+}
