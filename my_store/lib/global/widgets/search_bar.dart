0c950047a7a4288b9481a9f625baf65ba101e33b
George Amuzu <gnamuzu@gmail.com>
2024-03-12 23:39:32 +0000

    created search bar widget

---
+++ my_store/lib/global/widgets/search_bar.dart
@@ -0,0 +1,62 @@
+import 'package:flutter/material.dart';
+import 'package:iconsax/iconsax.dart';
+import 'package:my_store/utils/constants/colors.dart';
+import 'package:my_store/utils/constants/sizes.dart';
+import 'package:my_store/utils/device/devices.dart';
+import 'package:my_store/utils/helpers/helper_functions.dart';
+
+class SearchBarContainer extends StatelessWidget {
+  const SearchBarContainer({
+    super.key,
+    required this.text,
+    this.icon = Iconsax.search_normal,
+    this.showBackground = true,
+    this.showBorder = true,
+    this.onTap,
+  });
+
+  final String text;
+  final IconData? icon;
+  final bool showBackground, showBorder;
+  final VoidCallback? onTap;
+
+  @override
+  Widget build(BuildContext context) {
+    final dark = HelperFunctions.isDarkMode(context);
+
+    return GestureDetector(
+      onTap: onTap,
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: MySizes.defaultSpace),
+        child: Container(
+          width: MyDevices.getScreenWidth(context),
+          padding: const EdgeInsets.all(MySizes.md),
+          decoration: BoxDecoration(
+            color: showBackground
+                ? dark
+                    ? MyColors.dark
+                    : MyColors.light
+                : Colors.transparent,
+            borderRadius: BorderRadius.circular(MySizes.cardRadiusLg),
+            border: showBorder ? Border.all(color: MyColors.grey) : null,
+          ),
+          child: Row(
+            children: [
+              Icon(
+                icon,
+                color: MyColors.darkerGrey,
+              ),
+              const SizedBox(
+                width: MySizes.spaceBtwItems,
+              ),
+              Text(
+                text,
+                style: Theme.of(context).textTheme.bodySmall,
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
