80a8d6188307e4eba82f49b4186a39280f05cb9f
George Amuzu <gnamuzu@gmail.com>
2024-03-12 20:25:44 +0000

    added home screen

--- my_store/lib/features/navigation/controller/navigation_controller.dart
+++ my_store/lib/features/navigation/controller/navigation_controller.dart
@@ -1,12 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:my_store/features/home/screens/home.dart';
 
 class NavigationController extends GetxController {
   final Rx<int> selectedIndex = 0.obs;
   final screens = [
-    Container(
-      color: Colors.green,
-    ),
+    const HomeScreen(),
     Container(
       color: Colors.purple,
     ),
