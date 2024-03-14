6101771652fef6356d2d43fbe4c73ecdea1730cd
George Amuzu <gnamuzu@gmail.com>
2024-03-14 13:01:40 +0000

    added wishlist screen

--- my_store/lib/features/navigation/controller/navigation_controller.dart
+++ my_store/lib/features/navigation/controller/navigation_controller.dart
@@ -2,15 +2,14 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:my_store/features/home/screens/home.dart';
 import 'package:my_store/features/store/screens/store.dart';
+import 'package:my_store/features/wishlist/screens/wishlist.dart';
 
 class NavigationController extends GetxController {
   final Rx<int> selectedIndex = 0.obs;
   final screens = [
     const HomeScreen(),
     const StoreScreen(),
-    Container(
-      color: Colors.orange,
-    ),
+    const WishlistScreen(),
     Container(
       color: Colors.blue,
     ),
