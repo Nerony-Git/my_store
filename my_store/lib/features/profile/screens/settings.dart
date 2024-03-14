5e521de5ed6abbdcac8b2533626cbbf2b0d9327f
George Amuzu <gnamuzu@gmail.com>
2024-03-14 14:01:40 +0000

    created settings screen

---
+++ my_store/lib/features/profile/screens/settings.dart
@@ -0,0 +1,78 @@
+import 'package:flutter/material.dart';
+import 'package:iconsax/iconsax.dart';
+import 'package:my_store/features/home/widgets/header_container.dart';
+import 'package:my_store/features/profile/widgets/profile_tile.dart';
+import 'package:my_store/features/profile/widgets/settings_menu_tile.dart';
+import 'package:my_store/global/widgets/custom_app_bar.dart';
+import 'package:my_store/global/widgets/section_heading.dart';
+import 'package:my_store/utils/constants/colors.dart';
+import 'package:my_store/utils/constants/sizes.dart';
+
+class SettingsScreen extends StatelessWidget {
+  const SettingsScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            /// Header
+            HeaderContainer(
+              child: Column(
+                children: [
+                  /// AppBar
+                  CustomAppBar(
+                    title: Text('Account', style: Theme.of(context).textTheme.headlineMedium!.apply(color: MyColors.white),),
+                  ),
+                  const SizedBox(height: MySizes.spaceBtwSections,),
+
+                  /// Profile
+                  const ProfileTile(),
+                  const SizedBox(height: MySizes.spaceBtwSections,),
+                ],
+              ),
+            ),
+
+            /// Body
+            Padding(
+              padding: const EdgeInsets.all(MySizes.defaultSpace),
+              child: Column(
+                children: [
+                  /// Account settings
+                  const SectionHeading(title: 'Account Settings', showActionButton: false,),
+                  const SizedBox(height: MySizes.spaceBtwItems,),
+
+                  SettingsMenuTile(icon: Iconsax.safe_home, title: 'My Address', subtitle: 'Set shopping delivery address.', onTap: () {},),
+                  SettingsMenuTile(icon: Iconsax.shopping_cart, title: 'My Cart', subtitle: 'Add, remove products and move to checkout.', onTap: () {},),
+                  SettingsMenuTile(icon: Iconsax.bag_tick, title: 'My Orders', subtitle: 'In-progress and completed orders.', onTap: () {},),
+                  SettingsMenuTile(icon: Iconsax.bank, title: 'Bank Account', subtitle: 'Withdraw balance to registered bank account.', onTap: () {},),
+                  SettingsMenuTile(icon: Iconsax.discount_shape, title: 'My Coupons', subtitle: 'List of all discounted coupons.', onTap: () {},),
+                  SettingsMenuTile(icon: Iconsax.notification, title: 'Notification', subtitle: 'Set any kind of notification message.', onTap: () {},),
+                  SettingsMenuTile(icon: Iconsax.security_card, title: 'Account Privacy', subtitle: 'Manage data usage and connected accounts.', onTap: () {},),
+
+                  /// App Settings
+                  const SizedBox(height: MySizes.spaceBtwSections,),
+                  const SectionHeading(title: 'App Settings', showActionButton: false,),
+                  const SizedBox(height: MySizes.spaceBtwItems,),
+                  SettingsMenuTile(icon: Iconsax.document_upload, title: 'Load Data', subtitle: 'Upload Data.', onTap: () {},),
+                  SettingsMenuTile(icon: Iconsax.location, title: 'Location', subtitle: 'Set recommendations based on location.', trailing: Switch(value: true, onChanged: (value) {},),),
+                  SettingsMenuTile(icon: Iconsax.security_user, title: 'Safe Mode', subtitle: 'Search result is safe for all ages.', trailing: Switch(value: false, onChanged: (value) {},),),
+                  SettingsMenuTile(icon: Iconsax.image, title: 'HD Image Quality', subtitle: 'Set image quality to be seen.', trailing: Switch(value: false, onChanged: (value) {},),),
+
+                  /// Logout button
+                  const SizedBox(height: MySizes.spaceBtwSections,),
+                  SizedBox(
+                    width: double.infinity,
+                    child: OutlinedButton(onPressed: () {}, child: const Text('Logout'),),
+                  ),
+                  const SizedBox(height: MySizes.spaceBtwSections * 2.5,),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
