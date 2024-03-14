c6972ffbb6896ba52e5b791048c50f4d00db0953
George Amuzu <gnamuzu@gmail.com>
2024-03-14 12:43:24 +0000

    created category tab widget

---
+++ my_store/lib/features/store/widgets/category_tab.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+import 'package:my_store/features/store/widgets/brand_showcase.dart';
+import 'package:my_store/global/widgets/layouts/grid_layout.dart';
+import 'package:my_store/global/widgets/product/product_card_vertical.dart';
+import 'package:my_store/global/widgets/section_heading.dart';
+import 'package:my_store/utils/constants/images.dart';
+import 'package:my_store/utils/constants/sizes.dart';
+
+class CategoryTab extends StatelessWidget {
+  const CategoryTab({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView(
+      shrinkWrap: true,
+      physics: const NeverScrollableScrollPhysics(),
+      children: [
+        Padding(
+          padding: const EdgeInsets.all(MySizes.defaultSpace),
+          child: Column(
+            children: [
+              /// Brands
+              const BrandShowcase(imgs: [MyImages.productImage3, MyImages.productImage2, MyImages.productImage1,],),
+              const BrandShowcase(imgs: [MyImages.productImage3, MyImages.productImage2, MyImages.productImage1,],),
+              const SizedBox(height: MySizes.spaceBtwItems,),
+        
+              /// Products
+              SectionHeading(
+                title: 'You might like',
+                onPressed: () {},
+              ),
+              const SizedBox(height: MySizes.spaceBtwItems,),
+        
+              GridLayout(itemCount: 4, itemBuilder: (_, index) => const ProductCardVertical(),),
+              const SizedBox(height: MySizes.spaceBtwSections,),
+            ],
+          ),
+        ),
+      ],
+    );
+  }
+}
