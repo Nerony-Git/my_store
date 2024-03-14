53dee461058a68e5565b7373c8b884f254be52cc
George Amuzu <gnamuzu@gmail.com>
2024-03-14 07:02:13 +0000

    added store screen

--- my_store/lib/features/navigation/controller/navigation_controller.dart
+++ my_store/lib/features/navigation/controller/navigation_controller.dart
@@ -1,14 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:my_store/features/home/screens/home.dart';
+import 'package:my_store/features/store/screens/store.dart';
 
 class NavigationController extends GetxController {
   final Rx<int> selectedIndex = 0.obs;
   final screens = [
     const HomeScreen(),
-    Container(
-      color: Colors.purple,
-    ),
+    const StoreScreen(),
     Container(
       color: Colors.orange,
     ),
