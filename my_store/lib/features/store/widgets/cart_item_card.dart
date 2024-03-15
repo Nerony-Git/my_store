7a88677f3276f9878a192c7edcf823cfa8e1cb45
George Amuzu <gnamuzu@gmail.com>
2024-03-15 14:25:59 +0000

    created cart item card widget

---
+++ my_store/lib/features/store/widgets/cart_item_card.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+import 'package:my_store/global/widgets/imgs/round_image.dart';
+import 'package:my_store/global/widgets/product/brand_title_with_icon.dart';
+import 'package:my_store/global/widgets/product/product_title.dart';
+import 'package:my_store/utils/constants/colors.dart';
+import 'package:my_store/utils/constants/images.dart';
+import 'package:my_store/utils/constants/sizes.dart';
+import 'package:my_store/utils/helpers/helper_functions.dart';
+
+class CartItemCard extends StatelessWidget {
+  const CartItemCard({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    final dark = HelperFunctions.isDarkMode(context);
+
+    return Row(
+      children: [
+        /// Product image
+        RoundImage(
+          img: MyImages.productImage1,
+          width: 60,
+          height: 60,
+          padding: MySizes.sm,
+          backgroundColor: dark ? MyColors.darkerGrey : MyColors.light,
+        ),
+        const SizedBox(width: MySizes.spaceBtwItems,),
+    
+        /// Title, price and size
+        Expanded(
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const BrandTitleWithIcon(title: 'Nike'),
+              const Flexible(child: ProductTitle(title: 'Green Nike Air Shoes', maxLines: 1,)),
+              /// Attributes
+              Text.rich(
+                TextSpan(
+                  children: [
+                    TextSpan(text: 'Color', style: Theme.of(context).textTheme.bodySmall),
+                    TextSpan(text: 'Green', style: Theme.of(context).textTheme.bodyLarge),
+                    TextSpan(text: 'Size', style: Theme.of(context).textTheme.bodySmall),
+                    TextSpan(text: 'UK 10.5', style: Theme.of(context).textTheme.bodyLarge),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+      ],
+    );
+  }
+}
