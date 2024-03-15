639f40edfa96a92d219b11c5bb1fb8856b197a90
George Amuzu <gnamuzu@gmail.com>
2024-03-15 13:22:25 +0000

    created new address screen

---
+++ my_store/lib/features/profile/screens/new_address.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:iconsax/iconsax.dart';
+import 'package:my_store/global/widgets/custom_app_bar.dart';
+import 'package:my_store/utils/constants/sizes.dart';
+
+class NewAddressScreen extends StatelessWidget {
+  const NewAddressScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      /// Appbar
+      appBar: const CustomAppBar(title: Text('Add New Address'), showBackArrow: true,),
+
+      /// Body
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(MySizes.defaultSpace),
+          child: Form(
+            child: Column(
+              children: [
+                TextFormField(decoration: const InputDecoration(prefixIcon: Icon(Iconsax.user), labelText: 'Name'),),
+                const SizedBox(height: MySizes.spaceBtwInputFields,),
+                TextFormField(decoration: const InputDecoration(prefixIcon: Icon(Iconsax.mobile), labelText: 'Phone Number'),),
+                const SizedBox(height: MySizes.spaceBtwInputFields,),
+                Row(
+                  children: [
+                    Expanded(child: TextFormField(decoration: const InputDecoration(prefixIcon: Icon(Iconsax.building_31), labelText: 'Street'),)),
+                    const SizedBox(width: MySizes.spaceBtwInputFields,),
+                    Expanded(child: TextFormField(decoration: const InputDecoration(prefixIcon: Icon(Iconsax.code), labelText: 'Postal Code'),)),
+                    const SizedBox(height: MySizes.spaceBtwInputFields,),
+                  ],
+                ),
+                Row(
+                  children: [
+                    Expanded(child: TextFormField(decoration: const InputDecoration(prefixIcon: Icon(Iconsax.building), labelText: 'City'),)),
+                    const SizedBox(width: MySizes.spaceBtwInputFields,),
+                    Expanded(child: TextFormField(decoration: const InputDecoration(prefixIcon: Icon(Iconsax.activity), labelText: 'State'),)),
+                    const SizedBox(height: MySizes.spaceBtwInputFields,),
+                  ],
+                ),
+                TextFormField(decoration: const InputDecoration(prefixIcon: Icon(Iconsax.global), labelText: 'Country'),),
+                const SizedBox(height: MySizes.defaultSpace,),
+                SizedBox(width: double.infinity, child: ElevatedButton(onPressed: () {}, child: Text('Save'),),),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
