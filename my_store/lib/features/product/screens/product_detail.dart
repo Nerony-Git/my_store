4311e1f1ad9bb03f32d8302a18e922a2e65754a0
George Amuzu <gnamuzu@gmail.com>
2024-03-15 07:31:10 +0000

    added attributes, description, and reviews sections and add to cart bottom navigation

--- my_store/lib/features/product/screens/product_detail.dart
+++ my_store/lib/features/product/screens/product_detail.dart
@@ -1,33 +1,30 @@
 import 'package:flutter/material.dart';
 import 'package:iconsax/iconsax.dart';
+import 'package:my_store/features/product/widgets/bottom_add_to_cart.dart';
+import 'package:my_store/features/product/widgets/product_attributes.dart';
 import 'package:my_store/features/product/widgets/product_meta_data.dart';
 import 'package:my_store/features/product/widgets/rating_and_share.dart';
-import 'package:my_store/global/widgets/curved_edge.dart';
-import 'package:my_store/global/widgets/custom_app_bar.dart';
-import 'package:my_store/global/widgets/icons/rounded_icon.dart';
-import 'package:my_store/global/widgets/imgs/rounded_image.dart';
 import 'package:my_store/features/product/widgets/product_image_slider.dart';
-import 'package:my_store/utils/constants/colors.dart';
-import 'package:my_store/utils/constants/images.dart';
+import 'package:my_store/global/widgets/section_heading.dart';
 import 'package:my_store/utils/constants/sizes.dart';
-import 'package:my_store/utils/helpers/helper_functions.dart';
+import 'package:readmore/readmore.dart';
 
 class ProductDetailScreen extends StatelessWidget {
   const ProductDetailScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    final dark = HelperFunctions.isDarkMode(context);
-    return const Scaffold(
+    return Scaffold(
+      bottomNavigationBar: const BottomAddToCart(),
       body: SingleChildScrollView(
         child: Column(
           children: [
             /// Product images slider
-            ProductImageSlider(),
+            const ProductImageSlider(),
 
             /// Product details
             Padding(
-              padding: EdgeInsets.only(
+              padding: const EdgeInsets.only(
                 right: MySizes.defaultSpace,
                 left: MySizes.defaultSpace,
                 bottom: MySizes.defaultSpace,
@@ -35,15 +32,70 @@ class ProductDetailScreen extends StatelessWidget {
               child: Column(
                 children: [
                   /// Rating and Share button
-                  RatingAndShare(),
+                  const RatingAndShare(),
 
                   /// Price, title, stock and brand
-                  ProductMetaData(),
+                  const ProductMetaData(),
 
                   /// Attributes
+                  const ProductAttributes(),
+                  const SizedBox(
+                    height: MySizes.spaceBtwSections,
+                  ),
+
                   /// Checkout button
+                  SizedBox(
+                    width: double.infinity,
+                    child: ElevatedButton(
+                      onPressed: () {},
+                      child: const Text('Checkout'),
+                    ),
+                  ),
+                  const SizedBox(
+                    height: MySizes.spaceBtwSections,
+                  ),
+
                   /// Description
+                  const SectionHeading(
+                    title: 'Description',
+                    showActionButton: false,
+                  ),
+                  const SizedBox(
+                    height: MySizes.spaceBtwItems,
+                  ),
+                  const ReadMoreText(
+                    'This is a product description for Green Nike Shoes. There are more things that can be added here. There are more things that can be added here.',
+                    trimLines: 2,
+                    trimMode: TrimMode.Line,
+                    trimCollapsedText: ' more >>',
+                    trimExpandedText: ' << less',
+                    moreStyle:
+                        TextStyle(fontSize: 14, fontWeight: FontWeight.w800),
+                    lessStyle:
+                        TextStyle(fontSize: 14, fontWeight: FontWeight.w800),
+                  ),
+
                   /// Reviews
+                  const Divider(),
+                  const SizedBox(
+                    height: MySizes.spaceBtwItems,
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      const SectionHeading(
+                        title: 'Reviews (321)',
+                        showActionButton: false,
+                      ),
+                      IconButton(
+                        onPressed: () {},
+                        icon: const Icon(
+                          Iconsax.arrow_right_3,
+                          size: 18,
+                        ),
+                      ),
+                    ],
+                  ),
                 ],
               ),
             ),
