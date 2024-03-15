5123ac6ba0be409a5160e5e5d14904598fcdf171
George Amuzu <gnamuzu@gmail.com>
2024-03-15 12:31:05 +0000

    created rating progress indicator widget

---
+++ my_store/lib/features/product/widgets/rating_progress.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+import 'package:my_store/utils/constants/colors.dart';
+import 'package:my_store/utils/device/devices.dart';
+
+class RatingProgress extends StatelessWidget {
+  const RatingProgress({
+    super.key, required this.text, required this.value,
+  });
+
+  final String text;
+  final double value;
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        Expanded(flex: 1, child: Text(text, style: Theme.of(context).textTheme.bodyMedium,)),
+        Expanded(
+          flex: 11,
+          child: SizedBox(
+            width: MyDevices.getScreenWidth(context) * 0.8,
+            child: LinearProgressIndicator(
+              value: value,
+              minHeight: 11,
+              backgroundColor: MyColors.grey,
+              borderRadius: BorderRadius.circular(7),
+              valueColor: const AlwaysStoppedAnimation(MyColors.primary),
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
