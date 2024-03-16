0a0b9860b6652ec69511ce05e05e4eabfb7ccd7f
George Amuzu <gnamuzu@gmail.com>
2024-03-16 00:57:43 +0000

    modified to include void call back fro address, cart and orders

--- my_store/lib/features/profile/screens/settings.dart
+++ my_store/lib/features/profile/screens/settings.dart
@@ -2,10 +2,12 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:iconsax/iconsax.dart';
 import 'package:my_store/features/home/widgets/header_container.dart';
+import 'package:my_store/features/order/screens/order.dart';
 import 'package:my_store/features/profile/screens/address.dart';
 import 'package:my_store/features/profile/screens/profile.dart';
 import 'package:my_store/features/profile/widgets/profile_tile.dart';
 import 'package:my_store/features/profile/widgets/settings_menu_tile.dart';
+import 'package:my_store/features/store/screens/cart.dart';
 import 'package:my_store/global/widgets/custom_app_bar.dart';
 import 'package:my_store/global/widgets/section_heading.dart';
 import 'package:my_store/utils/constants/colors.dart';
@@ -26,13 +28,24 @@ class SettingsScreen extends StatelessWidget {
                 children: [
                   /// AppBar
                   CustomAppBar(
-                    title: Text('Account', style: Theme.of(context).textTheme.headlineMedium!.apply(color: MyColors.white),),
+                    title: Text(
+                      'Account',
+                      style: Theme.of(context)
+                          .textTheme
+                          .headlineMedium!
+                          .apply(color: MyColors.white),
+                    ),
                   ),
-                  const SizedBox(height: MySizes.spaceBtwSections,),
+                  const SizedBox(
+                    height: MySizes.spaceBtwSections,
+                  ),
 
                   /// Profile
-                  ProfileTile(onPressed: () => Get.to(() => const ProfileScreen())),
-                  const SizedBox(height: MySizes.spaceBtwSections,),
+                  ProfileTile(
+                      onPressed: () => Get.to(() => const ProfileScreen())),
+                  const SizedBox(
+                    height: MySizes.spaceBtwSections,
+                  ),
                 ],
               ),
             ),
@@ -43,33 +56,116 @@ class SettingsScreen extends StatelessWidget {
               child: Column(
                 children: [
                   /// Account settings
-                  const SectionHeading(title: 'Account Settings', showActionButton: false,),
-                  const SizedBox(height: MySizes.spaceBtwItems,),
+                  const SectionHeading(
+                    title: 'Account Settings',
+                    showActionButton: false,
+                  ),
+                  const SizedBox(
+                    height: MySizes.spaceBtwItems,
+                  ),
 
-                  SettingsMenuTile(icon: Iconsax.safe_home, title: 'My Address', subtitle: 'Set shopping delivery address.', onTap: () => Get.to(() => const AddressScreen()),),
-                  SettingsMenuTile(icon: Iconsax.shopping_cart, title: 'My Cart', subtitle: 'Add, remove products and move to checkout.', onTap: () {},),
-                  SettingsMenuTile(icon: Iconsax.bag_tick, title: 'My Orders', subtitle: 'In-progress and completed orders.', onTap: () {},),
-                  SettingsMenuTile(icon: Iconsax.bank, title: 'Bank Account', subtitle: 'Withdraw balance to registered bank account.', onTap: () {},),
-                  SettingsMenuTile(icon: Iconsax.discount_shape, title: 'My Coupons', subtitle: 'List of all discounted coupons.', onTap: () {},),
-                  SettingsMenuTile(icon: Iconsax.notification, title: 'Notification', subtitle: 'Set any kind of notification message.', onTap: () {},),
-                  SettingsMenuTile(icon: Iconsax.security_card, title: 'Account Privacy', subtitle: 'Manage data usage and connected accounts.', onTap: () {},),
+                  SettingsMenuTile(
+                    icon: Iconsax.safe_home,
+                    title: 'My Address',
+                    subtitle: 'Set shopping delivery address.',
+                    onTap: () => Get.to(() => const AddressScreen()),
+                  ),
+                  SettingsMenuTile(
+                    icon: Iconsax.shopping_cart,
+                    title: 'My Cart',
+                    subtitle: 'Add, remove products and move to checkout.',
+                    onTap: () => Get.to(() => const CartScreen()),
+                  ),
+                  SettingsMenuTile(
+                    icon: Iconsax.bag_tick,
+                    title: 'My Orders',
+                    subtitle: 'In-progress and completed orders.',
+                    onTap: () => Get.to(() => const OrderScreen()),
+                  ),
+                  SettingsMenuTile(
+                    icon: Iconsax.bank,
+                    title: 'Bank Account',
+                    subtitle: 'Withdraw balance to registered bank account.',
+                    onTap: () {},
+                  ),
+                  SettingsMenuTile(
+                    icon: Iconsax.discount_shape,
+                    title: 'My Coupons',
+                    subtitle: 'List of all discounted coupons.',
+                    onTap: () {},
+                  ),
+                  SettingsMenuTile(
+                    icon: Iconsax.notification,
+                    title: 'Notification',
+                    subtitle: 'Set any kind of notification message.',
+                    onTap: () {},
+                  ),
+                  SettingsMenuTile(
+                    icon: Iconsax.security_card,
+                    title: 'Account Privacy',
+                    subtitle: 'Manage data usage and connected accounts.',
+                    onTap: () {},
+                  ),
 
                   /// App Settings
-                  const SizedBox(height: MySizes.spaceBtwSections,),
-                  const SectionHeading(title: 'App Settings', showActionButton: false,),
-                  const SizedBox(height: MySizes.spaceBtwItems,),
-                  SettingsMenuTile(icon: Iconsax.document_upload, title: 'Load Data', subtitle: 'Upload Data.', onTap: () {},),
-                  SettingsMenuTile(icon: Iconsax.location, title: 'Location', subtitle: 'Set recommendations based on location.', trailing: Switch(value: true, onChanged: (value) {},),),
-                  SettingsMenuTile(icon: Iconsax.security_user, title: 'Safe Mode', subtitle: 'Search result is safe for all ages.', trailing: Switch(value: false, onChanged: (value) {},),),
-                  SettingsMenuTile(icon: Iconsax.image, title: 'HD Image Quality', subtitle: 'Set image quality to be seen.', trailing: Switch(value: false, onChanged: (value) {},),),
+                  const SizedBox(
+                    height: MySizes.spaceBtwSections,
+                  ),
+                  const SectionHeading(
+                    title: 'App Settings',
+                    showActionButton: false,
+                  ),
+                  const SizedBox(
+                    height: MySizes.spaceBtwItems,
+                  ),
+                  SettingsMenuTile(
+                    icon: Iconsax.document_upload,
+                    title: 'Load Data',
+                    subtitle: 'Upload Data.',
+                    onTap: () {},
+                  ),
+                  SettingsMenuTile(
+                    icon: Iconsax.location,
+                    title: 'Location',
+                    subtitle: 'Set recommendations based on location.',
+                    trailing: Switch(
+                      value: true,
+                      onChanged: (value) {},
+                    ),
+                  ),
+                  SettingsMenuTile(
+                    icon: Iconsax.security_user,
+                    title: 'Safe Mode',
+                    subtitle: 'Search result is safe for all ages.',
+                    trailing: Switch(
+                      value: false,
+                      onChanged: (value) {},
+                    ),
+                  ),
+                  SettingsMenuTile(
+                    icon: Iconsax.image,
+                    title: 'HD Image Quality',
+                    subtitle: 'Set image quality to be seen.',
+                    trailing: Switch(
+                      value: false,
+                      onChanged: (value) {},
+                    ),
+                  ),
 
                   /// Logout button
-                  const SizedBox(height: MySizes.spaceBtwSections,),
+                  const SizedBox(
+                    height: MySizes.spaceBtwSections,
+                  ),
                   SizedBox(
                     width: double.infinity,
-                    child: OutlinedButton(onPressed: () {}, child: const Text('Logout'),),
+                    child: OutlinedButton(
+                      onPressed: () {},
+                      child: const Text('Logout'),
+                    ),
                   ),
-                  const SizedBox(height: MySizes.spaceBtwSections * 2.5,),
+                  const SizedBox(
+                    height: MySizes.spaceBtwSections * 2.5,
+                  ),
                 ],
               ),
             ),
