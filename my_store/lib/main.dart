d6b391c448785960738e891dd532ee045233162f
George Amuzu <gnamuzu@gmail.com>
2024-03-16 19:39:46 +0000

    initialized widgets binding and GetX local storage.

--- my_store/lib/main.dart
+++ my_store/lib/main.dart
@@ -1,12 +1,18 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:get_storage/get_storage.dart';
 import 'package:my_store/app.dart';
 import 'package:my_store/firebase_options.dart';
 
 Future<void> main() async {
-  // TODO: Add Widgets Binding
-  // TODO: Init Local Storage
+  /// Add Widgets Binding
+  final WidgetsBinding widgetsBinding =
+      WidgetsFlutterBinding.ensureInitialized();
+
+  /// Initialize GetX Local Storage
+  await GetStorage.init();
+
   // TODO: Await Native Splash
 
   /// Initialized Firebase
