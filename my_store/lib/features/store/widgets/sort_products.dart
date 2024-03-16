f2f53d425b6caf277246170b840e51e878ac5a2a
George Amuzu <gnamuzu@gmail.com>
2024-03-16 11:48:31 +0000

    created sort products widget

---
+++ my_store/lib/features/store/widgets/sort_products.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+import 'package:iconsax/iconsax.dart';
+import 'package:my_store/global/widgets/layouts/grid_layout.dart';
+import 'package:my_store/global/widgets/product/product_card_vertical.dart';
+import 'package:my_store/utils/constants/sizes.dart';
+
+class SortProducts extends StatelessWidget {
+  const SortProducts({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        /// Filter
+        DropdownButtonFormField(
+          decoration: const InputDecoration(
+            prefixIcon: Icon(Iconsax.sort),
+            hintText: 'Sort By',
+          ),
+          items: [
+            'Name',
+            'High - Low price',
+            'Low - High Price',
+            'Sale',
+            'Newest',
+            'Popularity',
+          ]
+              .map(
+                (option) => DropdownMenuItem(
+                  value: option,
+                  child: Text(option),
+                ),
+              )
+              .toList(),
+          onChanged: (value) {},
+        ),
+        const SizedBox(
+          height: MySizes.spaceBtwSections,
+        ),
+
+        /// Products
+        GridLayout(
+          itemCount: 8,
+          itemBuilder: (_, index) => const ProductCardVertical(),
+        ),
+      ],
+    );
+  }
+}
