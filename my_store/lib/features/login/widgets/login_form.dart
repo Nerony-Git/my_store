fb9b57ca3ff90cc2b4fdf8055d53fa8bbedbcb11
George Amuzu <gnamuzu@gmail.com>
2024-03-17 18:27:59 +0000

    modified to include controller properties

--- my_store/lib/features/login/widgets/login_form.dart
+++ my_store/lib/features/login/widgets/login_form.dart
@@ -1,11 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:iconsax/iconsax.dart';
-import 'package:my_store/features/navigation/widgets/navigation_menu.dart';
+import 'package:my_store/features/login/controllers/login_controller.dart';
 import 'package:my_store/features/password/screens/forget_password.dart';
 import 'package:my_store/features/signup/screens/signup.dart';
 import 'package:my_store/utils/constants/sizes.dart';
 import 'package:my_store/utils/constants/texts.dart';
+import 'package:my_store/utils/validators/validator.dart';
 
 class LoginForm extends StatelessWidget {
   const LoginForm({
@@ -14,13 +15,18 @@ class LoginForm extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final controller = Get.put(LoginController());
+
     return Form(
+      key: controller.loginFormKey,
       child: Padding(
         padding: const EdgeInsets.symmetric(vertical: MySizes.spaceBtwSections),
         child: Column(
           children: [
             /// Email
             TextFormField(
+              validator: (value) => MyValidator.validateEmail(value),
+              controller: controller.email,
               decoration: const InputDecoration(
                 prefixIcon: Icon(Iconsax.direct),
                 labelText: MyText.email,
@@ -31,11 +37,25 @@ class LoginForm extends StatelessWidget {
             ),
 
             /// Password
-            TextFormField(
-              decoration: const InputDecoration(
-                prefixIcon: Icon(Iconsax.password_check),
-                labelText: MyText.password,
-                suffixIcon: Icon(Iconsax.eye_slash),
+            Obx(
+              () => TextFormField(
+                validator: (value) =>
+                    MyValidator.validateEmptyText('Password', value),
+                controller: controller.password,
+                obscureText: controller.hidePassword.value,
+                decoration: InputDecoration(
+                  prefixIcon: const Icon(Iconsax.password_check),
+                  labelText: MyText.password,
+                  suffixIcon: IconButton(
+                    onPressed: () => controller.hidePassword.value =
+                        !controller.hidePassword.value,
+                    icon: Icon(
+                      controller.hidePassword.value
+                          ? Iconsax.eye_slash
+                          : Iconsax.eye,
+                    ),
+                  ),
+                ),
               ),
             ),
             const SizedBox(
@@ -49,9 +69,12 @@ class LoginForm extends StatelessWidget {
                 /// Remember me
                 Row(
                   children: [
-                    Checkbox(
-                      value: true,
-                      onChanged: (value) {},
+                    Obx(
+                      () => Checkbox(
+                        value: controller.rememberMe.value,
+                        onChanged: (value) => controller.rememberMe.value =
+                            !controller.rememberMe.value,
+                      ),
                     ),
                     const Text(MyText.rememberMe),
                   ],
@@ -72,7 +95,8 @@ class LoginForm extends StatelessWidget {
             SizedBox(
               width: double.infinity,
               child: ElevatedButton(
-                onPressed: () => Get.to(() => const NavigationMenu()),
+                onPressed: () =>
+                    Get.to(() => controller.emailAndPasswordSignIn()),
                 child: const Text(MyText.signIn),
               ),
             ),
@@ -83,7 +107,7 @@ class LoginForm extends StatelessWidget {
             /// Sign up button
             SizedBox(
               width: double.infinity,
-              child: ElevatedButton(
+              child: OutlinedButton(
                 onPressed: () => Get.to(() => const SignupScreen()),
                 child: const Text(MyText.createAccount),
               ),
