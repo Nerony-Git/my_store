23ae8784b576e399da6cf3d179d601481e8a65aa
George Amuzu <gnamuzu@gmail.com>
2024-03-17 18:25:29 +0000

    modified to initialize authentication

--- my_store/lib/main.dart
+++ my_store/lib/main.dart
@@ -1,8 +1,10 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_native_splash/flutter_native_splash.dart';
 import 'package:get/get.dart';
 import 'package:get_storage/get_storage.dart';
 import 'package:my_store/app.dart';
+import 'package:my_store/data/repositories/authentication/authentication_repository.dart';
 import 'package:my_store/firebase_options.dart';
 
 Future<void> main() async {
@@ -13,14 +15,17 @@ Future<void> main() async {
   /// Initialize GetX Local Storage
   await GetStorage.init();
 
-  // TODO: Await Native Splash
+  /// Await Native Splash
+  FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
 
   /// Initialized Firebase
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
+  ).then(
+    /// Initialize Authentication
+    (FirebaseApp value) => Get.put(AuthenticationRepository()),
   );
 
-  // TODO: Initialize Authentication
-
+  /// Load all material design, themes, localizations and bindings.
   runApp(const App());
 }
