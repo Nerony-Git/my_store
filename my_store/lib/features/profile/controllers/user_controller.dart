0444737b3c27d31260bc439f65204162abc4d800
George Amuzu <gnamuzu@gmail.com>
2024-03-20 15:18:14 +0000

    added function to reauthenticate and delete user account

--- my_store/lib/features/profile/controllers/user_controller.dart
+++ my_store/lib/features/profile/controllers/user_controller.dart
@@ -1,7 +1,14 @@
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:my_store/data/repositories/authentication/authentication_repository.dart';
 import 'package:my_store/data/repositories/user/user_repository.dart';
+import 'package:my_store/features/login/screens/login.dart';
+import 'package:my_store/features/login/widgets/re_authenticate_user_login.dart';
+import 'package:my_store/utils/constants/images.dart';
+import 'package:my_store/utils/constants/sizes.dart';
 import 'package:my_store/utils/models/user_model.dart';
+import 'package:my_store/utils/popups/fullscreen_loader.dart';
 import 'package:my_store/utils/popups/loaders.dart';
 
 class UserController extends GetxController {
@@ -10,7 +17,12 @@ class UserController extends GetxController {
   /// Variables
   final profileLoading = false.obs;
   Rx<UserModel> user = UserModel.empty().obs;
+
+  final hidePassword = false.obs;
+  final verifyEmail = TextEditingController();
+  final verifyPassword = TextEditingController();
   final userRepository = Get.put(UserRepository());
+  GlobalKey<FormState> reAuthFormKey = GlobalKey<FormState>();
 
   @override
   void onInit() {
@@ -64,4 +76,53 @@ class UserController extends GetxController {
       profileLoading.value = false;
     }
   }
+
+  /// Delete account warining pop up
+  void deleteAccountWarningPopup() {
+    Get.defaultDialog(
+      contentPadding: const EdgeInsets.all(MySizes.md),
+      title: 'Delete Account',
+      middleText: 'Are you sure you want to delete your caaount permanently? This action is not reversible and all of your data will be removed permanently.',
+      confirm: ElevatedButton(
+        onPressed: () async => deleteUserAccount(),
+        style: ElevatedButton.styleFrom(backgroundColor: Colors.red, side: const BorderSide(color: Colors.red),), 
+        child: const Padding(padding: EdgeInsets.symmetric(horizontal: MySizes.lg), child: Text('Delete'),),
+      ),
+      cancel: OutlinedButton(onPressed: () => Navigator.of(Get.overlayContext!).pop(), child: const Text('Cancel'),),
+    );
+  }
+
+  /// Delete user account
+  void deleteUserAccount() async {
+    try {
+      FullScreenLoader.openLoadingDialog('Processing...', MyImages.docerAnimation);
+
+      /// Re-authenticate user
+      final auth = AuthenticationRepository.instance;
+      final provider = auth.authUser!.providerData.map((e) => e.providerId).first;
+      if (provider.isNotEmpty) {
+        if (provider == 'google.com') {
+          await auth.signInWithGoogle();
+          await auth.deleteAccount();
+          FullScreenLoader.stopLoading();
+          Get.offAll(() => const LoginScreen());
+        } else if (provider == 'password') {
+          FullScreenLoader.stopLoading();
+          Get.to(() => const ReAuthLoginForm());
+        }
+      }
+    } catch (e) {
+      /// Remove loader
+      FullScreenLoader.stopLoading();
+
+      /// Show some generic error to the user
+      SnackBars.errorSnackBar(
+        title: 'Oh Snap',
+        message: e.toString(),
+      );
+    }
+  }
+
+  /// Reauthenticate before deleting
+  Future<void> reAuthenticateEmailAndPasswordUser() async {}
 }
