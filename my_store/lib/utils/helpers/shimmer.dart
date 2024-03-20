a9cada65cb2cf3b10b97f9948f6049c2ff09cb56
George Amuzu <gnamuzu@gmail.com>
2024-03-20 14:26:39 +0000

    created a shimmer widget

---
+++ my_store/lib/utils/helpers/shimmer.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+import 'package:my_store/utils/constants/colors.dart';
+import 'package:my_store/utils/helpers/helper_functions.dart';
+import 'package:shimmer/shimmer.dart';
+
+class ShimmerEffect extends StatelessWidget {
+  const ShimmerEffect({
+    Key? key, 
+    required this.width, 
+    required this.height, 
+    this.radius = 15, 
+    this.color,
+  }) : super(key: key);
+
+  final double width, height, radius;
+  final Color? color;
+
+  @override
+  Widget build(BuildContext context) {
+    final dark = HelperFunctions.isDarkMode(context);
+
+    return Shimmer.fromColors(
+      baseColor: dark ? Colors.grey[850]! : Colors.grey[300]!, 
+      highlightColor: dark ? Colors.grey[700]! : Colors.grey[100]!,
+      child: Container(
+        width: width,
+        height: height,
+        decoration: BoxDecoration(
+          color: color ?? (dark ? MyColors.darkerGrey : MyColors.white),
+          borderRadius: BorderRadius.circular(radius),
+        ),
+      ), 
+    );
+  }
+}
