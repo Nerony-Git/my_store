794b04568fe1445054948c93f119c67008d4323f
George Amuzu <gnamuzu@gmail.com>
2024-03-20 12:45:43 +0000

    modified to include controller

--- my_store/lib/features/password/screens/forget_password.dart
+++ my_store/lib/features/password/screens/forget_password.dart
@@ -1,15 +1,18 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:iconsax/iconsax.dart';
-import 'package:my_store/features/password/screens/reset_password.dart';
+import 'package:my_store/features/password/controllers/forget_password_controller.dart';
 import 'package:my_store/utils/constants/sizes.dart';
 import 'package:my_store/utils/constants/texts.dart';
+import 'package:my_store/utils/validators/validator.dart';
 
 class ForgetPassword extends StatelessWidget {
   const ForgetPassword({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final controller = Get.put(ForgetPasswordController());
+
     return Scaffold(
       appBar: AppBar(),
       body: Padding(
@@ -34,10 +37,15 @@ class ForgetPassword extends StatelessWidget {
             ),
 
             /// Text field
-            TextFormField(
-              decoration: const InputDecoration(
-                labelText: MyText.email,
-                prefixIcon: Icon(Iconsax.direct),
+            Form(
+              key: controller.forgetPasswordFormKey,
+              child: TextFormField(
+                validator: MyValidator.validateEmail,
+                controller: controller.email,
+                decoration: const InputDecoration(
+                  labelText: MyText.email,
+                  prefixIcon: Icon(Iconsax.direct),
+                ),
               ),
             ),
             const SizedBox(
@@ -48,7 +56,7 @@ class ForgetPassword extends StatelessWidget {
             SizedBox(
               width: double.infinity,
               child: ElevatedButton(
-                onPressed: () => Get.off(() => const ResetPassword()),
+                onPressed: () => controller.sendPasswordResetEmail(),
                 child: const Text(MyText.submit),
               ),
             ),
