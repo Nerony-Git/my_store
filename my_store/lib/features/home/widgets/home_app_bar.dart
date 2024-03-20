d0846b12a67959f5a0149b64e92e6692d42b2203
George Amuzu <gnamuzu@gmail.com>
2024-03-20 14:24:25 +0000

    modified to use dynamic name

--- my_store/lib/features/home/widgets/home_app_bar.dart
+++ my_store/lib/features/home/widgets/home_app_bar.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:my_store/features/profile/controllers/user_controller.dart';
 import 'package:my_store/features/store/widgets/cart_icon.dart';
 import 'package:my_store/global/widgets/custom_app_bar.dart';
 import 'package:my_store/utils/constants/colors.dart';
 import 'package:my_store/utils/constants/texts.dart';
+import 'package:my_store/utils/helpers/shimmer.dart';
 
 class HomeAppBar extends StatelessWidget {
   const HomeAppBar({
@@ -11,6 +14,8 @@ class HomeAppBar extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final controller = Get.put(UserController());
+
     return CustomAppBar(
       title: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
@@ -22,12 +27,20 @@ class HomeAppBar extends StatelessWidget {
                 .labelMedium!
                 .apply(color: MyColors.grey),
           ),
-          Text(
-            MyText.homeAppbarSubTitle,
-            style: Theme.of(context)
-                .textTheme
-                .headlineSmall!
-                .apply(color: MyColors.white),
+          Obx(() {
+            if (controller.profileLoading.value) {
+              // Display a shimmer while user profile is loading
+              return const ShimmerEffect(width: 80, height: 15,);
+            } else {
+              return Text(
+                controller.user.value.fullName,
+                style: Theme.of(context)
+                    .textTheme
+                    .headlineSmall!
+                    .apply(color: MyColors.white),
+              );
+            }
+          }
           ),
         ],
       ),
