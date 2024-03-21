595e0b4795c770e8a3e240265f30980d39c4f77c
George Amuzu <gnamuzu@gmail.com>
2024-03-21 13:59:29 +0000

    modified to make section dynamic and adding shimmer effect

--- my_store/lib/features/home/widgets/home_categories.dart
+++ my_store/lib/features/home/widgets/home_categories.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:my_store/features/store/controllers/category_controller.dart';
 import 'package:my_store/features/store/screens/sub_category.dart';
+import 'package:my_store/global/widgets/shimmers/category_shimmer.dart';
 import 'package:my_store/global/widgets/vertical_categories.dart';
-import 'package:my_store/utils/constants/images.dart';
 
 class HomeCategories extends StatelessWidget {
   const HomeCategories({
@@ -11,20 +12,33 @@ class HomeCategories extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return SizedBox(
-      height: 80,
-      child: ListView.builder(
-        shrinkWrap: true,
-        itemCount: 6,
-        scrollDirection: Axis.horizontal,
-        itemBuilder: (_, index) {
-          return VerticalCategories(
-            img: MyImages.clothIcon,
-            title: 'Cloth',
-            onTap: () => Get.to(() => const SubCategoryScreen()),
-          );
-        },
-      ),
-    );
+    final controller = Get.put(CategoryController());
+
+    return Obx(() {
+      // Check if categoreis is loading
+      if (controller.isLoading.value) {
+        return const CategoryShimmer();
+      }
+      // Check id featured category is empty
+      if (controller.featuredCategories.isEmpty) {
+        return Center(child: Text('No Data Found', style: Theme.of(context).textTheme.bodyMedium!.apply(color: Colors.white),),);
+      }
+      return SizedBox(
+        height: 80,
+        child: ListView.builder(
+          shrinkWrap: true,
+          itemCount: controller.featuredCategories.length,
+          scrollDirection: Axis.horizontal,
+          itemBuilder: (_, index) {
+            final category = controller.featuredCategories[index];
+            return VerticalCategories(
+              img: category.img,
+              title: category.name,
+              onTap: () => Get.to(() => const SubCategoryScreen()),
+            );
+          },
+        ),
+      );
+    });
   }
 }
