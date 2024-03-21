72ed7fb8feab4582dc8c71a2c43c78e02ba823fb
George Amuzu <gnamuzu@gmail.com>
2024-03-21 11:20:28 +0000

    added cached network image

--- my_store/lib/global/widgets/imgs/round_image.dart
+++ my_store/lib/global/widgets/imgs/round_image.dart
@@ -1,7 +1,9 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:my_store/utils/constants/colors.dart';
 import 'package:my_store/utils/constants/sizes.dart';
 import 'package:my_store/utils/helpers/helper_functions.dart';
+import 'package:my_store/utils/helpers/shimmer.dart';
 
 class RoundImage extends StatelessWidget {
   const RoundImage({
@@ -35,13 +37,23 @@ class RoundImage extends StatelessWidget {
                 : MyColors.white),
         borderRadius: BorderRadius.circular(100),
       ),
-      child: Center(
-        child: Image(
-            fit: fit,
-            image: isNetworkImage
-                ? NetworkImage(img)
-                : AssetImage(img) as ImageProvider,
-            color: overlayColor),
+      child: ClipRRect(
+        borderRadius: BorderRadius.circular(100),
+        child: Center(
+          child: isNetworkImage 
+              ? CachedNetworkImage(
+                  fit: fit, 
+                  color: overlayColor, 
+                  imageUrl: img, 
+                  progressIndicatorBuilder: (context, url, progress) => const ShimmerEffect(width: 55, height: 55, radius: 55,),
+                  errorWidget: (context, url, error) => const Icon(Icons.error),
+                  )
+              : Image(
+                  fit: fit,
+                  image: AssetImage(img),
+                  color: overlayColor,
+                ),
+        ),
       ),
     );
   }
