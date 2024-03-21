eebb00d6488a2cc1ae5ff8e5da97dc820b94e01b
George Amuzu <gnamuzu@gmail.com>
2024-03-21 14:01:51 +0000

    created custom firebase storage service

---
+++ my_store/lib/utils/helpers/firebase_storage_service.dart
@@ -0,0 +1,70 @@
+import 'dart:io';
+
+import 'package:firebase_storage/firebase_storage.dart';
+import 'package:flutter/services.dart';
+import 'package:get/get.dart';
+import 'package:image_picker/image_picker.dart';
+
+class CustomFirebaseStorageService extends GetxController {
+  static CustomFirebaseStorageService get instance => Get.find();
+
+  /// Variables
+  final _firebaseStorage = FirebaseStorage.instance;
+
+  /// Upload local assets from IDE
+  /// Returns a Unit8List containing image data
+  Future<Uint8List> getImageDataFromAssets(String path) async {
+    try {
+      final byteData = await rootBundle.load(path);
+      final imageData = byteData.buffer.asUint8List(byteData.offsetInBytes, byteData.lengthInBytes);
+      return imageData;
+    } catch (e) {
+      // Handel exceptions
+      throw 'Error loading image data: $e';
+    }
+  }
+
+  /// Upload image using image data on cloud firebase storage
+  /// Returns the url of the uploaded image.
+  Future<String> uploadImageData(String path, Uint8List image, String name) async {
+    try {
+      final ref = _firebaseStorage.ref(path).child(name);
+      await ref.putData(image);
+      final url = await ref.getDownloadURL();
+      return url;
+    } catch (e) {
+      // Handle excepions
+      if (e is FirebaseException) {
+        throw 'Firebase Exception: ${e.message}';
+      } else if (e is SocketException) {
+        throw 'Network Error: ${e.message}';
+      } else if (e is PlatformException) {
+        throw 'Platform Exception: ${e.message}';
+      } else {
+        'Something went wrong! Please try again.';
+      }
+    }
+  }
+
+  /// Upload image on firebase storage
+  /// Returns the url of the uploaded image.
+  Future<String> uploadImageFile(String path, XFile image) async {
+    try {
+      final ref = _firebaseStorage.ref(path).child(image.name);
+      await ref.putFile(File(image.path));
+      final url = await ref.getDownloadURL();
+      return url;
+    } catch (e) {
+      // Handle excepions
+      if (e is FirebaseException) {
+        throw 'Firebase Exception: ${e.message}';
+      } else if (e is SocketException) {
+        throw 'Network Error: ${e.message}';
+      } else if (e is PlatformException) {
+        throw 'Platform Exception: ${e.message}';
+      } else {
+        'Something went wrong! Please try again.';
+      }
+    }
+  }
+}
