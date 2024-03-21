f22da033f4a8b1716c8076b04f8ed65542cbd924
George Amuzu <gnamuzu@gmail.com>
2024-03-21 11:18:36 +0000

    added upload image function

--- my_store/lib/data/repositories/user/user_repository.dart
+++ my_store/lib/data/repositories/user/user_repository.dart
@@ -1,6 +1,10 @@
+import 'dart:io';
+
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/services.dart';
 import 'package:get/get.dart';
+import 'package:image_picker/image_picker.dart';
 import 'package:my_store/data/repositories/authentication/authentication_repository.dart';
 import 'package:my_store/utils/exceptions/firebase_exceptions.dart';
 import 'package:my_store/utils/exceptions/format_exceptions.dart';
@@ -81,6 +85,28 @@ class UserRepository extends GetxController {
   Future<void> deleteUserData(String userID) async {
     try {
       await _db.collection("Users").doc(userID).delete();
+    } on FirebaseException catch (e) {
+      throw CustomFirebaseException(e.code).message;
+    } on FormatException catch (_) {
+      throw const CustomFormatException();
+    } on PlatformException catch (e) {
+      throw CustomPlatformException(e.code).message;
+    } catch (e) {
+      throw 'Something went wrong. Please try again!';
+    }
+  }
+
+  /// Function to upload images
+  Future<String> uploadImage(String path, XFile img) async {
+    try {
+      final ref = FirebaseStorage.instance.ref(path).child(img.name);
+
+      // Upload image to folder
+      await ref.putFile(File(img.path));
+
+      // Get the image url
+      final url = await ref.getDownloadURL();
+      return url;
     } on FirebaseException catch (e) {
       throw CustomFirebaseException(e.code).message;
     } on FormatException catch (_) {
