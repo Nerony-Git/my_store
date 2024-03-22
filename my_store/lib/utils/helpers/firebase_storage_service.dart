7b15e6ba1f6cc5e20b0e8d443f4ca89e56f0579b
George Amuzu <gnamuzu@gmail.com>
2024-03-22 07:11:06 +0000

    modified to include throw

--- my_store/lib/utils/helpers/firebase_storage_service.dart
+++ my_store/lib/utils/helpers/firebase_storage_service.dart
@@ -16,7 +16,8 @@ class CustomFirebaseStorageService extends GetxController {
   Future<Uint8List> getImageDataFromAssets(String path) async {
     try {
       final byteData = await rootBundle.load(path);
-      final imageData = byteData.buffer.asUint8List(byteData.offsetInBytes, byteData.lengthInBytes);
+      final imageData = byteData.buffer
+          .asUint8List(byteData.offsetInBytes, byteData.lengthInBytes);
       return imageData;
     } catch (e) {
       // Handel exceptions
@@ -26,14 +27,15 @@ class CustomFirebaseStorageService extends GetxController {
 
   /// Upload image using image data on cloud firebase storage
   /// Returns the url of the uploaded image.
-  Future<String> uploadImageData(String path, Uint8List image, String name) async {
+  Future<String> uploadImageData(
+      String path, Uint8List image, String name) async {
     try {
       final ref = _firebaseStorage.ref(path).child(name);
       await ref.putData(image);
       final url = await ref.getDownloadURL();
       return url;
     } catch (e) {
-      // Handle excepions
+      // Handle exceptions
       if (e is FirebaseException) {
         throw 'Firebase Exception: ${e.message}';
       } else if (e is SocketException) {
@@ -41,7 +43,7 @@ class CustomFirebaseStorageService extends GetxController {
       } else if (e is PlatformException) {
         throw 'Platform Exception: ${e.message}';
       } else {
-        'Something went wrong! Please try again.';
+        throw 'Something went wrong! Please try again.';
       }
     }
   }
@@ -63,7 +65,7 @@ class CustomFirebaseStorageService extends GetxController {
       } else if (e is PlatformException) {
         throw 'Platform Exception: ${e.message}';
       } else {
-        'Something went wrong! Please try again.';
+        throw 'Something went wrong! Please try again.';
       }
     }
   }
