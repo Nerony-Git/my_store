0260e223eb0f23d9a5b69e83ec1d6faa8856acca
George Amuzu <gnamuzu@gmail.com>
2024-03-23 20:43:15 +0000

    added function to fetch banners

--- my_store/lib/data/repositories/banner/banner_repository.dart
+++ my_store/lib/data/repositories/banner/banner_repository.dart
@@ -1,5 +1,12 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/services.dart';
 import 'package:get/get.dart';
+import 'package:my_store/utils/exceptions/firebase_auth_exceptions.dart';
+import 'package:my_store/utils/exceptions/firebase_exceptions.dart';
+import 'package:my_store/utils/exceptions/format_exceptions.dart';
+import 'package:my_store/utils/exceptions/platform_exceptions.dart';
+import 'package:my_store/utils/models/banner_model.dart';
 
 class BannerRepository extends GetxController {
   static BannerRepository get instance => Get.find();
@@ -8,4 +15,25 @@ class BannerRepository extends GetxController {
   final _db = FirebaseFirestore.instance;
 
   /// Get all banners
+  Future<List<BannerModel>> fetchBanners() async {
+    try {
+      final result = await _db
+          .collection('Banners')
+          .where('Active', isEqualTo: true)
+          .get();
+      return result.docs
+          .map((documentSnapshot) => BannerModel.fromSnapshot(documentSnapshot))
+          .toList();
+    } on FirebaseAuthException catch (e) {
+      throw CustomFirebaseAuthException(e.code).message;
+    } on FirebaseException catch (e) {
+      throw CustomFirebaseException(e.code).message;
+    } on FormatException catch (_) {
+      throw const CustomFormatException();
+    } on PlatformException catch (e) {
+      throw CustomPlatformException(e.code).message;
+    } catch (e) {
+      throw 'Something went wrong. Please try again!';
+    }
+  }
 }
