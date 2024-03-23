5d334a39283c7ca4de5d6cac3ec6e81ec5ffc083
George Amuzu <gnamuzu@gmail.com>
2024-03-23 20:46:29 +0000

    modified to fetch banner upon initialization

--- my_store/lib/features/home/controllers/slider_controller.dart
+++ my_store/lib/features/home/controllers/slider_controller.dart
@@ -1,4 +1,6 @@
 import 'package:get/get.dart';
+import 'package:my_store/data/repositories/banner/banner_repository.dart';
+import 'package:my_store/utils/models/banner_model.dart';
 import 'package:my_store/utils/popups/loaders.dart';
 
 class SliderController extends GetxController {
@@ -7,17 +9,31 @@ class SliderController extends GetxController {
   /// Variables
   final isLoading = false.obs;
   final carouselCurrentIndex = 0.obs;
+  final RxList<BannerModel> banners = <BannerModel>[].obs;
+
+  @override
+  void onInit() {
+    fetchBanners();
+    super.onInit();
+  }
 
   /// Update page navigational dots
   void updatePageIndicator(index) {
     carouselCurrentIndex.value = index;
   }
 
-  /// Function to load category data.
+  /// Function to load banners.
   Future<void> fetchBanners() async {
     try {
       // Show loader while loading categories
       isLoading.value = true;
+
+      // Fetch Banners
+      final bannerRepository = Get.put(BannerRepository());
+      final banners = await bannerRepository.fetchBanners();
+
+      // Assign Banners
+      this.banners.assignAll(banners);
     } catch (e) {
       // Show some generic error to the user
       SnackBars.errorSnackBar(
