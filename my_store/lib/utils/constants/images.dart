b49215f9546bdd8180bda3fa5cd6ad6b8f2cc36a
George Amuzu <gnamuzu@gmail.com>
2024-03-23 16:17:17 +0000

    added brand logos path

--- my_store/lib/utils/constants/images.dart
+++ my_store/lib/utils/constants/images.dart
@@ -48,6 +48,19 @@ class MyImages {
   static const String electronicsIcon =
       "assets/icons/categories/icons8-smartphone-64.png";
 
+  // -- Brand Icons
+  static const String nikeLogo = "assets/icons/brands/nike.png";
+  static const String adidasLogo = "assets/icons/brands/adidas-logo.png";
+  static const String appleLogo = "assets/icons/brands/apple-logo.png";
+  static const String jordanLogo = "assets/icons/brands/jordan-logo.png";
+  static const String pumaLogo = "assets/icons/brands/puma-logo.png";
+  static const String zaraLogo = "assets/icons/brands/zara-logo.png";
+  static const String kenwoodLogo = "assets/icons/brands/kenwood-logo.png";
+  static const String hermanMillerLogo =
+      "assets/icons/brands/herman-miller-logo.png";
+  static const String ikeaLogo = "assets/icons/brands/ikea_logo.png";
+  static const String acerlogo = "assets/icons/brands/acer_logo.png";
+
   // -- Banners
   static const String promoBanner1 = "assets/img/banners/promo-banner-1.png";
   static const String promoBanner2 = "assets/img/banners/promo-banner-2.png";
