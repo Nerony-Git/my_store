6a83377fdb7423c0c2d9c8bc7a0501ce97a09d08
George Amuzu <gnamuzu@gmail.com>
2024-03-23 20:44:30 +0000

    corrected the spelling of categories

--- my_store/lib/data/repositories/category/category_repository.dart
+++ my_store/lib/data/repositories/category/category_repository.dart
@@ -11,12 +11,14 @@ class CategoryRepository extends GetxController {
 
   /// Variables
   final _db = FirebaseFirestore.instance;
-  
+
   /// Function to get all categories
   Future<List<CategoryModel>> getAllCategories() async {
     try {
       final snapshot = await _db.collection('Categories').get();
-      final list = snapshot.docs.map((document) => CategoryModel.fromSnapshot(document)).toList();
+      final list = snapshot.docs
+          .map((document) => CategoryModel.fromSnapshot(document))
+          .toList();
       return list;
     } on FirebaseException catch (e) {
       throw CustomFirebaseException(e.code).message;
@@ -26,26 +28,32 @@ class CategoryRepository extends GetxController {
       throw 'Something went wrong. Please try again!';
     }
   }
+
   /// Function to get sub categories
-  /// Function to upload catgories to the cloud firebase
-  Future<void> uploadDummyData(List<CategoryModel> categoreis) async {
+
+  /// Function to upload categories to the cloud firebase
+  Future<void> uploadDummyData(List<CategoryModel> categories) async {
     try {
-      // Upload all the categories along with thier images
+      // Upload all the categories along with their images
       final storage = Get.put(CustomFirebaseStorageService());
 
-      // Loop theough each category
-      for (var category in categoreis) {
-        // get image data link from tje local assets
+      // Loop through each category
+      for (var category in categories) {
+        // get image data link from the local assets
         final file = await storage.getImageDataFromAssets(category.img);
 
         // Upload image and get its url
-        final url = await storage.uploadImageData('Categories', file, category.name);
+        final url =
+            await storage.uploadImageData('Categories', file, category.name);
 
         // Assign URL to category image attribute
         category.img = url;
 
         // Store category in firestore
-        await _db.collection("Categories").doc(category.id).set(category.toJson());
+        await _db
+            .collection("Categories")
+            .doc(category.id)
+            .set(category.toJson());
       }
     } on FirebaseException catch (e) {
       throw CustomFirebaseException(e.code).message;
