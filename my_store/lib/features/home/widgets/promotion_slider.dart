85ef7bdef11ca81b7a70d74c0eb5459e21850e8b
George Amuzu <gnamuzu@gmail.com>
2024-03-23 20:48:00 +0000

    modified to make it dynamic

--- my_store/lib/features/home/widgets/promotion_slider.dart
+++ my_store/lib/features/home/widgets/promotion_slider.dart
@@ -6,50 +6,70 @@ import 'package:my_store/global/widgets/circular_container.dart';
 import 'package:my_store/global/widgets/imgs/rounded_image.dart';
 import 'package:my_store/utils/constants/colors.dart';
 import 'package:my_store/utils/constants/sizes.dart';
+import 'package:my_store/utils/helpers/shimmer.dart';
 
 class PromotionSlider extends StatelessWidget {
   const PromotionSlider({
     super.key,
-    required this.banners,
   });
-
-  final List<String> banners;
 
   @override
   Widget build(BuildContext context) {
     final controller = Get.put(SliderController());
 
-    return Column(
-      children: [
-        CarouselSlider(
-          items: banners.map((url) => RoundedImage(imageUrl: url)).toList(),
-          options: CarouselOptions(
-            viewportFraction: 1,
-            onPageChanged: (index, _) => controller.updatePageIndicator(index),
-          ),
-        ),
-        const SizedBox(
-          height: MySizes.spaceBtwItems,
-        ),
-        Center(
-          child: Obx(
-            () => Row(
-              mainAxisSize: MainAxisSize.min,
-              children: [
-                for (int i = 0; i < banners.length; i++)
-                  CircularContainer(
-                    width: 20,
-                    height: 4,
-                    margin: const EdgeInsets.only(right: 10),
-                    backgroundColor: controller.carouselCurrentIndex.value == i
-                        ? MyColors.primary
-                        : MyColors.grey,
-                  ),
-              ],
+    return Obx(() {
+      // Check if its loading
+      if (controller.isLoading.value) {
+        return const ShimmerEffect(width: double.infinity, height: 190);
+      }
+
+      // Check for no data found
+      if (controller.banners.isEmpty) {
+        return const Center(
+          child: Text('No Data Found!'),
+        );
+      } else {
+        return Column(
+          children: [
+            CarouselSlider(
+              items: controller.banners
+                  .map((banner) => RoundedImage(
+                        imageUrl: banner.imageUrl,
+                        isNetworkImage: true,
+                        onPressed: () => Get.toNamed(banner.targetScreen),
+                      ))
+                  .toList(),
+              options: CarouselOptions(
+                viewportFraction: 1,
+                onPageChanged: (index, _) =>
+                    controller.updatePageIndicator(index),
+              ),
             ),
-          ),
-        ),
-      ],
-    );
+            const SizedBox(
+              height: MySizes.spaceBtwItems,
+            ),
+            Center(
+              child: Obx(
+                () => Row(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    for (int i = 0; i < controller.banners.length; i++)
+                      CircularContainer(
+                        width: 20,
+                        height: 4,
+                        margin: const EdgeInsets.only(right: 10),
+                        backgroundColor:
+                            controller.carouselCurrentIndex.value == i
+                                ? MyColors.primary
+                                : MyColors.grey,
+                      ),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        );
+      }
+    });
   }
 }
