37528bd9b1c76a5c674bb470a830062f8e11dbbc
George Amuzu <gnamuzu@gmail.com>
2024-03-23 21:07:03 +0000

    added function to upload product data

--- my_store/lib/utils/repositories/upload_data_repository.dart
+++ my_store/lib/utils/repositories/upload_data_repository.dart
@@ -1,10 +1,14 @@
+import 'dart:io';
+
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/services.dart';
 import 'package:get/get.dart';
+import 'package:my_store/utils/constants/enums.dart';
 import 'package:my_store/utils/exceptions/firebase_exceptions.dart';
 import 'package:my_store/utils/exceptions/platform_exceptions.dart';
 import 'package:my_store/utils/helpers/firebase_storage_service.dart';
 import 'package:my_store/utils/models/banner_model.dart';
+import 'package:my_store/utils/models/product_model.dart';
 
 class UploadDataRepository extends GetxController {
   static UploadDataRepository get instance => Get.find();
@@ -29,7 +33,7 @@ class UploadDataRepository extends GetxController {
         final url =
             await storage.uploadImageData('assets/img/banners', file, fileName);
 
-        // Assign URL to category image attribute
+        // Assign URL to banner image attribute
         banner.imageUrl = url;
 
         // Store banner data in Firestore
@@ -37,6 +41,82 @@ class UploadDataRepository extends GetxController {
       }
     } on FirebaseException catch (e) {
       throw CustomFirebaseException(e.code).message;
+    } on PlatformException catch (e) {
+      throw CustomPlatformException(e.code).message;
+    } catch (e) {
+      throw 'Something went wrong. Please try again!';
+    }
+  }
+
+  /// Function to upload dummy product data to cloud Firebase
+  Future<void> uploadProductData(List<ProductModel> products) async {
+    try {
+      // Upload all the products along with their images
+      final storage = Get.put(CustomFirebaseStorageService());
+
+      // Loop through each product
+      for (var product in products) {
+        // Get image data link from local assets
+        final thumbnail =
+            await storage.getImageDataFromAssets(product.thumbnail);
+
+        // Get the image name
+        final thumbnailName = product.thumbnail.split('/').last;
+
+        // Upload image and get its url
+        final url = await storage.uploadImageData(
+            'assets/img/products', thumbnail, thumbnailName);
+
+        // Assign url to product thumbnail attribute
+        product.thumbnail = url;
+
+        // Product list of images
+        if (product.images != null && product.images!.isNotEmpty) {
+          List<String> imageUrl = [];
+          for (var image in product.images!) {
+            // Get image data link from local assets
+            final assetImage = await storage.getImageDataFromAssets(image);
+
+            // Get the image name
+            final assetImageName = image.split('/').last;
+
+            // Upload image and get its url
+            final url = await storage.uploadImageData(
+                'assets/img/products', assetImage, assetImageName);
+
+            // Assign url to product thumbnail attribute
+            imageUrl.add(url);
+          }
+          product.images!.clear();
+          product.images!.addAll(imageUrl);
+        }
+
+        // Upload variation images
+        if (product.productType == ProductType.variable.toString()) {
+          for (var variation in product.productVariations!) {
+            // Get image data link from local assets
+            final assetImage =
+                await storage.getImageDataFromAssets(variation.image);
+
+            // get the image name
+            final assetImageName = variation.image.split('/').last;
+
+            // Upload image and get it's url
+            final url = await storage.uploadImageData(
+                'assets/img/products', assetImage, assetImageName);
+
+            // Assign url to variation image attribute
+            variation.image = url;
+          }
+        }
+
+        // Stor product in Firestore
+        await _db.collection('Products').doc(product.id).set(product.toJson());
+      }
+    } on FirebaseException catch (e) {
+      throw CustomFirebaseException(e.code).message;
+    } on SocketException catch (e) {
+      throw e.message;
     } on PlatformException catch (e) {
       throw CustomPlatformException(e.code).message;
     } catch (e) {
