572e72fa1341780b5a059d568e1a3f85a0f2fba6
George Amuzu <gnamuzu@gmail.com>
2024-03-24 15:10:48 +0000

    added function to upload product dummy data

--- my_store/lib/utils/controllers/upload_data_controller.dart
+++ my_store/lib/utils/controllers/upload_data_controller.dart
@@ -2,6 +2,7 @@ import 'package:get/get.dart';
 import 'package:my_store/utils/constants/images.dart';
 import 'package:my_store/utils/helpers/network_manager.dart';
 import 'package:my_store/utils/models/banner_model.dart';
+import 'package:my_store/utils/models/product_model.dart';
 import 'package:my_store/utils/popups/fullscreen_loader.dart';
 import 'package:my_store/utils/popups/loaders.dart';
 import 'package:my_store/utils/repositories/upload_data_repository.dart';
@@ -17,7 +18,7 @@ class UploadDataController extends GetxController {
     try {
       /// Start Loading
       FullScreenLoader.openLoadingDialog(
-          'We are uploading your banners...', MyImages.docerAnimation);
+          'We are uploading your banners...', MyImages.cloudUploadingAnimation);
 
       /// Check internet connectivity
       final isConnected = await NetworkManager.instance.isConnected();
@@ -35,6 +36,42 @@ class UploadDataController extends GetxController {
       SnackBars.successSnackBar(
         title: 'Success',
         message: 'Banners uploaded successfully.',
+      );
+    } catch (e) {
+      /// Remove loader
+      FullScreenLoader.stopLoading();
+
+      /// Show some generic error to the user
+      SnackBars.errorSnackBar(
+        title: 'Oh Snap',
+        message: e.toString(),
+      );
+    }
+  }
+
+  /// Upload Products
+  Future<void> uploadProducts(List<ProductModel> products) async {
+    try {
+      /// Start Loading
+      FullScreenLoader.openLoadingDialog('We are uploading your products...',
+          MyImages.cloudUploadingAnimation);
+
+      /// Check internet connectivity
+      final isConnected = await NetworkManager.instance.isConnected();
+      if (!isConnected) {
+        FullScreenLoader.stopLoading();
+        return;
+      }
+
+      await _uploadDataRepository.uploadProductData(products);
+
+      /// Remove loader
+      FullScreenLoader.stopLoading();
+
+      /// Show success message
+      SnackBars.successSnackBar(
+        title: 'Success',
+        message: 'Products uploaded successfully.',
       );
     } catch (e) {
       /// Remove loader
