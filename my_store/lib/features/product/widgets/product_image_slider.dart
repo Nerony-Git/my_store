1a850f39e36699827c2646eb94300b881463da34
George Amuzu <gnamuzu@gmail.com>
2024-03-24 15:08:30 +0000

    modified to be dynamic

--- my_store/lib/features/product/widgets/product_image_slider.dart
+++ my_store/lib/features/product/widgets/product_image_slider.dart
@@ -1,21 +1,29 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 import 'package:iconsax/iconsax.dart';
+import 'package:my_store/features/product/controllers/product_image_controller.dart';
 import 'package:my_store/global/widgets/curved_edge.dart';
 import 'package:my_store/global/widgets/custom_app_bar.dart';
 import 'package:my_store/global/widgets/icons/rounded_icon.dart';
 import 'package:my_store/global/widgets/imgs/rounded_image.dart';
 import 'package:my_store/utils/constants/colors.dart';
-import 'package:my_store/utils/constants/images.dart';
 import 'package:my_store/utils/constants/sizes.dart';
 import 'package:my_store/utils/helpers/helper_functions.dart';
+import 'package:my_store/utils/models/product_model.dart';
 
 class ProductImageSlider extends StatelessWidget {
   const ProductImageSlider({
     super.key,
+    required this.product,
   });
+
+  final ProductModel product;
 
   @override
   Widget build(BuildContext context) {
+    final controller = Get.put(ProductImagesController());
+    final images = controller.getAllProductImages(product);
     final dark = HelperFunctions.isDarkMode(context);
 
     return CurvedEdge(
@@ -23,26 +31,26 @@ class ProductImageSlider extends StatelessWidget {
         color: dark ? MyColors.darkerGrey : MyColors.light,
         child: Stack(
           children: [
-            /// AppBar icons
-            const CustomAppBar(
-              showBackArrow: true,
-              actions: [
-                RoundedIcon(
-                  icon: Iconsax.heart5,
-                  color: Colors.red,
-                ),
-              ],
-            ),
-
             /// Main large image
-            const SizedBox(
+            SizedBox(
               height: 400,
               child: Padding(
-                padding: EdgeInsets.all(MySizes.productImageRadius * 2),
+                padding: const EdgeInsets.all(MySizes.productImageRadius * 2),
                 child: Center(
-                  child: Image(
-                    image: AssetImage(MyImages.productImage1),
-                  ),
+                  child: Obx(() {
+                    final image = controller.selectedProductImage.value;
+                    return GestureDetector(
+                      onTap: () => controller.showEnlargedImage(image),
+                      child: CachedNetworkImage(
+                        imageUrl: image,
+                        progressIndicatorBuilder: (_, __, downloadProgress) =>
+                            CircularProgressIndicator(
+                          value: downloadProgress.progress,
+                          color: MyColors.primary,
+                        ),
+                      ),
+                    );
+                  }),
                 ),
               ),
             ),
@@ -55,22 +63,43 @@ class ProductImageSlider extends StatelessWidget {
               child: SizedBox(
                 height: 80,
                 child: ListView.separated(
+                  itemCount: images.length,
                   shrinkWrap: true,
                   scrollDirection: Axis.horizontal,
                   physics: const AlwaysScrollableScrollPhysics(),
-                  itemBuilder: (_, index) => RoundedImage(
-                    width: 80,
-                    backgroundColor: dark ? MyColors.dark : MyColors.white,
-                    border: Border.all(color: MyColors.primary),
-                    padding: const EdgeInsets.all(MySizes.sm),
-                    imageUrl: MyImages.productImage1,
-                  ),
                   separatorBuilder: (_, __) => const SizedBox(
                     width: MySizes.spaceBtwItems,
                   ),
-                  itemCount: 6,
+                  itemBuilder: (_, index) => Obx(() {
+                    final imageSelected =
+                        controller.selectedProductImage.value == images[index];
+                    return RoundedImage(
+                      width: 80,
+                      isNetworkImage: true,
+                      imageUrl: images[index],
+                      padding: const EdgeInsets.all(MySizes.sm),
+                      backgroundColor: dark ? MyColors.dark : MyColors.white,
+                      onPressed: () =>
+                          controller.selectedProductImage.value = images[index],
+                      border: Border.all(
+                          color: imageSelected
+                              ? MyColors.primary
+                              : Colors.transparent),
+                    );
+                  }),
                 ),
               ),
+            ),
+
+            /// AppBar icons
+            const CustomAppBar(
+              showBackArrow: true,
+              actions: [
+                RoundedIcon(
+                  icon: Iconsax.heart5,
+                  color: Colors.red,
+                ),
+              ],
             ),
           ],
         ),
