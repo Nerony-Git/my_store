1f3fae0b1b8bfa9043ea58e3cb49d8160bd2c588
George Amuzu <gnamuzu@gmail.com>
2024-03-29 17:41:01 +0000

    added function to get categories brands

--- my_store/lib/data/repositories/brand/brand_repository.dart
+++ my_store/lib/data/repositories/brand/brand_repository.dart
@@ -34,4 +34,41 @@ class BrandRepository extends GetxController {
       throw 'Something went wrong. Please try again!';
     }
   }
+
+  /// Get brands for category
+  Future<List<BrandModel>> getBrandsForCategory(String categoryID) async {
+    try {
+      // Query to get all documents where categoryID matches the provided categoryID
+      QuerySnapshot brandCategoryQuery = await _db
+          .collection('BrandCategory')
+          .where('categoryID', isEqualTo: categoryID)
+          .get();
+
+      // Extract brand IDs from the documents
+      List<String> brandIDs = brandCategoryQuery.docs
+          .map((doc) => doc['brandID'] as String)
+          .toList();
+
+      // Query to get all documents where the brandID is in the list of brandIDs, FieldPath.documentID to query documents in collection
+      final brandsQuery = await _db
+          .collection('Brands')
+          .where(FieldPath.documentId, whereIn: brandIDs)
+          .limit(2)
+          .get();
+
+      // Extract brand names or other relevant data from the documents
+      List<BrandModel> brands =
+          brandsQuery.docs.map((doc) => BrandModel.fromSnapshot(doc)).toList();
+
+      return brands;
+    } on FirebaseException catch (e) {
+      throw CustomFirebaseException(e.code).message;
+    } on FormatException catch (_) {
+      throw const CustomFormatException();
+    } on PlatformException catch (e) {
+      throw CustomPlatformException(e.code).message;
+    } catch (e) {
+      throw 'Something went wrong. Please try again!';
+    }
+  }
 }
