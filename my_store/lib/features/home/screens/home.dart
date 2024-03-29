24d91761ffd8112ddc7c19e8d6f4937e5e24f296
George Amuzu <gnamuzu@gmail.com>
2024-03-29 12:18:37 +0000

    added void call function

--- my_store/lib/features/home/screens/home.dart
+++ my_store/lib/features/home/screens/home.dart
@@ -82,7 +82,14 @@ class HomeScreen extends StatelessWidget {
                   /// Popular Products Section
                   SectionHeading(
                     title: 'Popular Products',
-                    onPressed: () => Get.to(() => const AllProductsScreen()),
+                    onPressed: () => Get.to(() => AllProductsScreen(
+                          title: 'Popular Products',
+                          // Using query
+                          // query: FirebaseFirestore.instance.collection('Products').where('IsFeatured', isEqualTo: true).limit(6),
+
+                          // Using a future method
+                          futureMethod: controller.fetchAllFeaturedProducts(),
+                        )),
                   ),
                   const SizedBox(
                     height: MySizes.spaceBtwItems,
