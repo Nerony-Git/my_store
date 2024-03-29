fc7b492752152ee0674478e43ed197de63c86830
George Amuzu <gnamuzu@gmail.com>
2024-03-29 12:22:47 +0000

    modified to be dynamic

--- my_store/lib/features/store/widgets/brand_card.dart
+++ my_store/lib/features/store/widgets/brand_card.dart
@@ -1,22 +1,22 @@
 import 'package:flutter/material.dart';
-import 'package:get/get.dart';
-import 'package:my_store/features/store/screens/brand.dart';
 import 'package:my_store/global/widgets/containers/rounded_container.dart';
 import 'package:my_store/global/widgets/imgs/round_image.dart';
 import 'package:my_store/global/widgets/product/brand_title_with_icon.dart';
 import 'package:my_store/utils/constants/colors.dart';
 import 'package:my_store/utils/constants/enums.dart';
-import 'package:my_store/utils/constants/images.dart';
 import 'package:my_store/utils/constants/sizes.dart';
 import 'package:my_store/utils/helpers/helper_functions.dart';
+import 'package:my_store/utils/models/brand_model.dart';
 
 class BrandCard extends StatelessWidget {
   const BrandCard({
     super.key,
     this.showBorder = true,
     this.onTap,
+    required this.brand,
   });
 
+  final BrandModel brand;
   final bool showBorder;
   final void Function()? onTap;
 
@@ -25,7 +25,7 @@ class BrandCard extends StatelessWidget {
     final dark = HelperFunctions.isDarkMode(context);
 
     return GestureDetector(
-      onTap: () => Get.to(() => const BrandScreen()),
+      onTap: onTap,
       child: RoundedContainer(
         padding: const EdgeInsets.all(MySizes.sm),
         showBorder: showBorder,
@@ -35,8 +35,8 @@ class BrandCard extends StatelessWidget {
             /// Icon
             Flexible(
               child: RoundImage(
-                isNetworkImage: false,
-                img: MyImages.clothIcon,
+                isNetworkImage: true,
+                img: brand.image,
                 backgroundColor: Colors.transparent,
                 overlayColor: dark ? MyColors.white : MyColors.black,
               ),
@@ -51,12 +51,12 @@ class BrandCard extends StatelessWidget {
                 mainAxisSize: MainAxisSize.min,
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
-                  const BrandTitleWithIcon(
-                    title: 'Nike',
+                  BrandTitleWithIcon(
+                    title: brand.name,
                     brandTextSize: TextSizes.large,
                   ),
                   Text(
-                    '349 products with ',
+                    '${brand.productsCount ?? 0} products',
                     overflow: TextOverflow.ellipsis,
                     style: Theme.of(context).textTheme.labelMedium,
                   ),
