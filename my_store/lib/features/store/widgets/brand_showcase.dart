dadf5960d5127dc673cd241e80d69044f279ed4f
George Amuzu <gnamuzu@gmail.com>
2024-03-29 17:43:32 +0000

    added an inkwell and cached network image widget

--- my_store/lib/features/store/widgets/brand_showcase.dart
+++ my_store/lib/features/store/widgets/brand_showcase.dart
@@ -1,40 +1,51 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:my_store/features/store/screens/brand.dart';
 import 'package:my_store/features/store/widgets/brand_card.dart';
 import 'package:my_store/global/widgets/containers/rounded_container.dart';
 import 'package:my_store/utils/constants/colors.dart';
 import 'package:my_store/utils/constants/sizes.dart';
 import 'package:my_store/utils/helpers/helper_functions.dart';
+import 'package:my_store/utils/helpers/shimmer.dart';
 import 'package:my_store/utils/models/brand_model.dart';
 
 class BrandShowcase extends StatelessWidget {
   const BrandShowcase({
     super.key,
     required this.imgs,
+    required this.brand,
   });
 
+  final BrandModel brand;
   final List<String> imgs;
 
   @override
   Widget build(BuildContext context) {
-    return RoundedContainer(
-      showBorder: true,
-      borderColor: MyColors.darkGrey,
-      backgroundColor: Colors.transparent,
-      padding: const EdgeInsets.all(MySizes.md),
-      margin: const EdgeInsets.only(bottom: MySizes.spaceBtwItems),
-      child: Column(
-        children: [
-          /// Brand with products count
-          BrandCard(
-            showBorder: false,
-            brand: BrandModel.empty(),
-          ),
+    return InkWell(
+      onTap: () => Get.to(() => BrandScreen(
+            brand: brand,
+          )),
+      child: RoundedContainer(
+        showBorder: true,
+        borderColor: MyColors.darkGrey,
+        backgroundColor: Colors.transparent,
+        padding: const EdgeInsets.all(MySizes.md),
+        margin: const EdgeInsets.only(bottom: MySizes.spaceBtwItems),
+        child: Column(
+          children: [
+            /// Brand with products count
+            BrandCard(
+              showBorder: false,
+              brand: brand,
+            ),
 
-          /// Brand's top 3 product images
-          Row(
-              children:
-                  imgs.map((img) => brandTopProducts(img, context)).toList()),
-        ],
+            /// Brand's top 3 product images
+            Row(
+                children:
+                    imgs.map((img) => brandTopProducts(img, context)).toList()),
+          ],
+        ),
       ),
     );
   }
@@ -48,7 +59,13 @@ class BrandShowcase extends StatelessWidget {
         backgroundColor: dark ? MyColors.darkerGrey : MyColors.light,
         margin: const EdgeInsets.only(right: MySizes.sm),
         padding: const EdgeInsets.all(MySizes.md),
-        child: Image(fit: BoxFit.contain, image: AssetImage(img)),
+        child: CachedNetworkImage(
+          fit: BoxFit.contain,
+          imageUrl: img,
+          progressIndicatorBuilder: (context, url, downloadProgress) =>
+              const ShimmerEffect(width: 100, height: 100),
+          errorWidget: (context, url, error) => const Icon(Icons.error),
+        ),
       ),
     );
   }
