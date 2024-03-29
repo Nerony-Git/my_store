1b0cbbb19b779719bdc31355cb1ce89746e73fde
George Amuzu <gnamuzu@gmail.com>
2024-03-29 12:22:29 +0000

    modified to be dynamic

--- my_store/lib/features/store/screens/store.dart
+++ my_store/lib/features/store/screens/store.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:my_store/features/store/controllers/brand_controller.dart';
 import 'package:my_store/features/store/controllers/category_controller.dart';
 import 'package:my_store/features/store/screens/all_brands.dart';
+import 'package:my_store/features/store/screens/brand.dart';
 import 'package:my_store/features/store/widgets/brand_card.dart';
 import 'package:my_store/features/store/widgets/category_tab.dart';
 import 'package:my_store/global/widgets/appbars/custom_tab_bar.dart';
@@ -10,6 +12,7 @@ import 'package:my_store/global/widgets/custom_app_bar.dart';
 import 'package:my_store/global/widgets/layouts/grid_layout.dart';
 import 'package:my_store/global/widgets/search_bar.dart';
 import 'package:my_store/global/widgets/section_heading.dart';
+import 'package:my_store/global/widgets/shimmers/brand_shimmer.dart';
 import 'package:my_store/utils/constants/colors.dart';
 import 'package:my_store/utils/constants/sizes.dart';
 import 'package:my_store/utils/helpers/helper_functions.dart';
@@ -19,6 +22,7 @@ class StoreScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final brandController = Get.put(BrandController());
     final categories = CategoryController.instance.featuredCategories;
 
     return DefaultTabController(
@@ -71,27 +75,63 @@ class StoreScreen extends StatelessWidget {
                         height: MySizes.spaceBtwItems / 1.5,
                       ),
 
-                      GridLayout(
-                        itemCount: 4,
-                        mainAxisExtent: 80,
-                        itemBuilder: (_, index) {
-                          return const BrandCard();
-                        },
-                      ),
+                      /// Brand grid
+                      Obx(() {
+                        // Check if brand is loading
+                        if (brandController.isLoading.value) {
+                          return const BrandShimmer();
+                        }
+
+                        // Check if featured brands is empty
+                        if (brandController.featuredBrands.isEmpty) {
+                          return Center(
+                            child: Text(
+                              'No Data Found!',
+                              style: Theme.of(context)
+                                  .textTheme
+                                  .bodyMedium!
+                                  .apply(color: Colors.white),
+                            ),
+                          );
+                        }
+
+                        // Return featured brand list
+                        return GridLayout(
+                          itemCount: brandController.featuredBrands.length,
+                          mainAxisExtent: 80,
+                          itemBuilder: (_, index) {
+                            // Select brand
+                            final brand = brandController.featuredBrands[index];
+                            return BrandCard(
+                              brand: brand,
+                              onTap: () => Get.to(() => BrandScreen(
+                                    brand: brand,
+                                  )),
+                            );
+                          },
+                        );
+                      }),
                     ],
                   ),
                 ),
 
                 // Tabs Section
                 bottom: CustomTabBar(
-                  tabs: categories.map((category) => Tab(child: Text(category.name),)).toList(),
+                  tabs: categories
+                      .map((category) => Tab(
+                            child: Text(category.name),
+                          ))
+                      .toList(),
                 ),
               ),
             ];
           },
 
           /// Body
-          body: TabBarView(children: categories.map((category) => CategoryTab(category: category)).toList()),
+          body: TabBarView(
+              children: categories
+                  .map((category) => CategoryTab(category: category))
+                  .toList()),
         ),
       ),
     );
