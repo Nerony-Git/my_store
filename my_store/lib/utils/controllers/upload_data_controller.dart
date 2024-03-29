538d2c36baa544073f52c1e14faca35c6f72f1ce
George Amuzu <gnamuzu@gmail.com>
2024-03-29 17:46:33 +0000

    added function to upload category, brand and product category data

--- my_store/lib/utils/controllers/upload_data_controller.dart
+++ my_store/lib/utils/controllers/upload_data_controller.dart
@@ -2,7 +2,10 @@ import 'package:get/get.dart';
 import 'package:my_store/utils/constants/images.dart';
 import 'package:my_store/utils/helpers/network_manager.dart';
 import 'package:my_store/utils/models/banner_model.dart';
+import 'package:my_store/utils/models/brand_category_model.dart';
 import 'package:my_store/utils/models/brand_model.dart';
+import 'package:my_store/utils/models/category_model.dart';
+import 'package:my_store/utils/models/product_category_model.dart';
 import 'package:my_store/utils/models/product_model.dart';
 import 'package:my_store/utils/popups/fullscreen_loader.dart';
 import 'package:my_store/utils/popups/loaders.dart';
@@ -109,6 +112,117 @@ class UploadDataController extends GetxController {
       SnackBars.successSnackBar(
         title: 'Success',
         message: 'Brands uploaded successfully.',
+      );
+    } catch (e) {
+      /// Remove loader
+      FullScreenLoader.stopLoading();
+
+      /// Show some generic error to the user
+      SnackBars.errorSnackBar(
+        title: 'Oh Snap',
+        message: e.toString(),
+      );
+    }
+  }
+
+  /// Upload Product Category
+  Future<void> uploadProductCategory(
+      List<ProductCategoryModel> categories) async {
+    try {
+      /// Start Loading
+      FullScreenLoader.openLoadingDialog(
+          'We are uploading your product category relation data...',
+          MyImages.cloudUploadingAnimation);
+
+      /// Check internet connectivity
+      final isConnected = await NetworkManager.instance.isConnected();
+      if (!isConnected) {
+        FullScreenLoader.stopLoading();
+        return;
+      }
+
+      await _uploadDataRepository.uploadProductCategory(categories);
+
+      /// Remove loader
+      FullScreenLoader.stopLoading();
+
+      /// Show success message
+      SnackBars.successSnackBar(
+        title: 'Success',
+        message: 'Product Category relation data uploaded successfully.',
+      );
+    } catch (e) {
+      /// Remove loader
+      FullScreenLoader.stopLoading();
+
+      /// Show some generic error to the user
+      SnackBars.errorSnackBar(
+        title: 'Oh Snap',
+        message: e.toString(),
+      );
+    }
+  }
+
+  /// Upload Brand Category
+  Future<void> uploadBrandCategory(List<BrandCategoryModel> categories) async {
+    try {
+      /// Start Loading
+      FullScreenLoader.openLoadingDialog(
+          'We are uploading your brand category relation data...',
+          MyImages.cloudUploadingAnimation);
+
+      /// Check internet connectivity
+      final isConnected = await NetworkManager.instance.isConnected();
+      if (!isConnected) {
+        FullScreenLoader.stopLoading();
+        return;
+      }
+
+      await _uploadDataRepository.uploadBrandCategory(categories);
+
+      /// Remove loader
+      FullScreenLoader.stopLoading();
+
+      /// Show success message
+      SnackBars.successSnackBar(
+        title: 'Success',
+        message: 'Brand Category relation data uploaded successfully.',
+      );
+    } catch (e) {
+      /// Remove loader
+      FullScreenLoader.stopLoading();
+
+      /// Show some generic error to the user
+      SnackBars.errorSnackBar(
+        title: 'Oh Snap',
+        message: e.toString(),
+      );
+    }
+  }
+
+  /// Upload Categories
+  Future<void> uploadCategories(List<CategoryModel> categories) async {
+    try {
+      /// Start Loading
+      FullScreenLoader.openLoadingDialog('We are uploading your categories...',
+          MyImages.cloudUploadingAnimation);
+
+      /// Check internet connectivity
+      final isConnected = await NetworkManager.instance.isConnected();
+      if (!isConnected) {
+        FullScreenLoader.stopLoading();
+        return;
+      }
+
+      await _uploadDataRepository.uploadCategoryData(categories);
+
+      /// Remove loader
+      FullScreenLoader.stopLoading();
+
+      /// Show success message
+      SnackBars.successSnackBar(
+        title: 'Success',
+        message: 'Categories uploaded successfully.',
       );
     } catch (e) {
       /// Remove loader
