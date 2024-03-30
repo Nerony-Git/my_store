46c10571df79acf07b32509c49e8f8644b063138
George Amuzu <gnamuzu@gmail.com>
2024-03-30 07:13:38 +0000

    modified to be dynamic

--- my_store/lib/features/profile/screens/address.dart
+++ my_store/lib/features/profile/screens/address.dart
@@ -1,17 +1,21 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:iconsax/iconsax.dart';
+import 'package:my_store/features/profile/controllers/address_controller.dart';
 import 'package:my_store/features/profile/screens/new_address.dart';
 import 'package:my_store/features/profile/widgets/address_card.dart';
 import 'package:my_store/global/widgets/custom_app_bar.dart';
 import 'package:my_store/utils/constants/colors.dart';
 import 'package:my_store/utils/constants/sizes.dart';
+import 'package:my_store/utils/helpers/cloud_functions.dart';
 
 class AddressScreen extends StatelessWidget {
   const AddressScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final controller = Get.put(AddressController());
+
     return Scaffold(
       /// Add new address button
       floatingActionButton: FloatingActionButton(
@@ -32,15 +36,33 @@ class AddressScreen extends StatelessWidget {
       ),
 
       /// Body
-      body: const SingleChildScrollView(
+      body: SingleChildScrollView(
         child: Padding(
-          padding: EdgeInsets.all(MySizes.defaultSpace),
-          child: Column(
-            children: [
-              AddressCard(selectedAddress: true),
-              AddressCard(selectedAddress: false),
-              AddressCard(selectedAddress: false),
-            ],
+          padding: const EdgeInsets.all(MySizes.defaultSpace),
+          child: Obx(
+            () => FutureBuilder(
+                // Use key to trigger refresh
+                key: Key(controller.refreshData.value.toString()),
+                future: controller.getAllUserAddresses(),
+                builder: (context, snapshot) {
+                  /// Handle loader, no record, or error message
+                  final widget = CloudFunctions.checkMultiRecordState(
+                    snapshot: snapshot,
+                  );
+                  if (widget != null) return widget;
+
+                  /// Record found
+                  final addresses = snapshot.data!;
+
+                  return ListView.builder(
+                    shrinkWrap: true,
+                    itemCount: addresses.length,
+                    itemBuilder: (_, index) => AddressCard(
+                      address: addresses[index],
+                      onTap: () => controller.selectAddress(addresses[index]),
+                    ),
+                  );
+                }),
           ),
         ),
       ),
