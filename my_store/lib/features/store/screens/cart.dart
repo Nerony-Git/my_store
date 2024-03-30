b4d5e1d968b2dcfa432267810d19d780b760d574
George Amuzu <gnamuzu@gmail.com>
2024-03-30 13:15:07 +0000

    modified screen to be dynamic

--- my_store/lib/features/store/screens/cart.dart
+++ my_store/lib/features/store/screens/cart.dart
@@ -1,8 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:my_store/features/navigation/widgets/navigation_menu.dart';
+import 'package:my_store/features/store/controllers/cart_controller.dart';
 import 'package:my_store/features/store/screens/checkout.dart';
 import 'package:my_store/features/store/widgets/cart_items.dart';
 import 'package:my_store/global/widgets/custom_app_bar.dart';
+import 'package:my_store/global/widgets/loaders/animation_loader.dart';
+import 'package:my_store/utils/constants/images.dart';
 import 'package:my_store/utils/constants/sizes.dart';
 
 class CartScreen extends StatelessWidget {
@@ -10,6 +14,8 @@ class CartScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final controller = CartController.instance;
+
     return Scaffold(
       appBar: CustomAppBar(
         title: Text(
@@ -18,21 +24,43 @@ class CartScreen extends StatelessWidget {
         ),
         showBackArrow: true,
       ),
-      body: const Padding(
-        padding: EdgeInsets.all(MySizes.defaultSpace),
+      body: Obx(
+        () {
+          /// Nothing found widget
+          final emptyWidget = AnimationLoader(
+            text: 'Whoops! Cart is Empty...',
+            animation: MyImages.cartAnimation,
+            showAction: true,
+            actionText: 'Let\'s fill it',
+            onActionPressed: () => Get.off(() => const NavigationMenu()),
+          );
 
-        /// Cart item
-        child: CartItems(),
+          if (controller.cartItems.isEmpty) {
+            return emptyWidget;
+          } else {
+            return const SingleChildScrollView(
+              child: Padding(
+                padding: EdgeInsets.all(MySizes.defaultSpace),
+
+                /// Cart item
+                child: CartItems(),
+              ),
+            );
+          }
+        },
       ),
 
       /// Checkout button
-      bottomNavigationBar: Padding(
-        padding: const EdgeInsets.all(MySizes.defaultSpace),
-        child: ElevatedButton(
-          onPressed: () => Get.to(() => const CheckoutScreen()),
-          child: const Text('Checkout £ 280.00'),
-        ),
-      ),
+      bottomNavigationBar: controller.cartItems.isEmpty
+          ? const SizedBox()
+          : Padding(
+              padding: const EdgeInsets.all(MySizes.defaultSpace),
+              child: ElevatedButton(
+                onPressed: () => Get.to(() => const CheckoutScreen()),
+                child: Obx(() => Text(
+                    'Checkout £${(controller.totalCartAmount.value).toStringAsFixed(2)}')),
+              ),
+            ),
     );
   }
 }
